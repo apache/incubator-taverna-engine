edbccf458568ae6117087b14f5f578f6afc5746a
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-04-24 09:50:41 +0000

    Added example value as a possibility for workflow input port Removed mimetype as a possibility for workflow input port
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@7395 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-api/src/main/java/net/sf/taverna/t2/annotation/annotationbeans/ExampleValue.java
+++ workflowmodel-api/src/main/java/net/sf/taverna/t2/annotation/annotationbeans/ExampleValue.java
@@ -21,40 +21,22 @@
 package net.sf.taverna.t2.annotation.annotationbeans;
 
 import net.sf.taverna.t2.annotation.AppliesTo;
-import net.sf.taverna.t2.workflowmodel.Port;
+import net.sf.taverna.t2.workflowmodel.DataflowInputPort;
 
 /**
- * A single MIME type, intended to be used to annotate an input or output port
- * within the workflow to denote the type within that system of data produced or
- * consumed by the port.
+ * A String containing an example or a description of an example
  * 
- * @author Tom Oinn
+ * @author Alan R Williams
  * 
  */
-// @AppliesTo(targetObjectType = { Port.class })
-public class MimeType extends AbstractTextualValueAssertion {
+@AppliesTo(targetObjectType = { DataflowInputPort.class }, many = false)
+public class ExampleValue extends AbstractTextualValueAssertion {
 
 	/**
 	 * Default constructor as mandated by java bean specification
 	 */
-	public MimeType() {
-		super();
-	}
-
-	/**
-	 * Return the MIME type as a string, mime types look like 'part/part'. We
-	 * may want to consider whether it's possible to make this a genuine
-	 * enumeration driven off a canonical list of MIME types or whether it's
-	 * best kept as the current (free) string. The advantage of an enumerated
-	 * type is that we could attach description to the MIME types which would
-	 * help with the UI construction but maybe this isn't the place to put it
-	 * (should this link be in the UI layer? probably)
-	 * 
-	 * @return the MIME type as a string.
-	 */
-	@Override
-	public String getText() {
-		return super.getText();
+	public ExampleValue() {
+		//
 	}
 
 }
