a0eb674f897fa4b1abd2fcbc1798d6aa5fb38e32
paolo@mygrid.org.uk <paolo@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-10-06 10:31:59 +0000

    added getters/setters for invocContext. refService
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@8381 bf327186-88b3-11dd-a302-d386e5130c1c

--- provenanceconnector/src/main/java/net/sf/taverna/t2/provenance/connector/ProvenanceConnector.java
+++ provenanceconnector/src/main/java/net/sf/taverna/t2/provenance/connector/ProvenanceConnector.java
@@ -58,322 +58,334 @@ import org.apache.log4j.Logger;
  */
 public abstract class ProvenanceConnector implements ProvenanceReporter {
 
-    private static Logger logger = Logger.getLogger(ProvenanceConnector.class);
-    private String saveEvents;    
-    private ProvenanceAnalysis provenanceAnalysis;
-    private ExecutorService executor = Executors.newSingleThreadExecutor();
-    private boolean isClearDB = false;
-    private Provenance provenance;
-    private boolean finished = false;
-    private String sessionID;
+	private static Logger logger = Logger.getLogger(ProvenanceConnector.class);
+	private String saveEvents;    
+	private ProvenanceAnalysis provenanceAnalysis;
+	private ExecutorService executor = Executors.newSingleThreadExecutor();
+	private boolean isClearDB = false;
+	private Provenance provenance;
+	private boolean finished = false;
+	private String sessionID;
 	private InvocationContext invocationContext;
 	private ReferenceService referenceService;
+	
+	public ProvenanceConnector() {
 
-    public ProvenanceConnector() {
-        
-    }
-
-    public ProvenanceConnector(Provenance provenance,
-            ProvenanceAnalysis provenanceAnalysis,
-            boolean isClearDB, String saveEvents) {
-
-        setProvenance(provenance);
-        this.setProvenanceAnalysis(provenanceAnalysis);
-        this.isClearDB = isClearDB;
-        this.saveEvents = saveEvents;
-        getProvenance().setSaveEvents(this.saveEvents);
-    }
-    
-   /**
-   * Uses a {@link ScheduledThreadPoolExecutor} to process events in a Thread
-   * safe manner
-   */
-  public synchronized void addProvenanceItem(
-          final ProvenanceItem provenanceItem) {
-
-      Runnable runnable = new Runnable() {
-
-          public void run() {
-              try {
-
-                  getProvenance().acceptRawProvenanceEvent(
-                          provenanceItem.getEventType(), provenanceItem);
-
-              } catch (SQLException e) {
-                  logger.warn("Could not add provenance for " + provenanceItem.getEventType() + " " + provenanceItem.getIdentifier() + " " + e);
-              } catch (IOException e) {
-                  logger.warn("Could not add provenance for " + provenanceItem.getEventType() + " " + provenanceItem.getIdentifier() + " " + e);
-              }
-
-          }
-      };
-      getExecutor().execute(runnable);
-
-  }
-
-    protected Connection getConnection() throws InstantiationException,
-            IllegalAccessException, ClassNotFoundException, SQLException {
-        return JDBCConnector.getConnection();
-    }
-
-    /**
-     * Used by database backed provenance stores. Ask the implementation to
-     * create the database. Requires each datbase type to create all its own
-     * tables
-     */
-    public abstract void createDatabase();    
-
-    /**
-     * Clear all the values in the database but keep the db there
-     */
-    public void clearDatabase() {
-        String q = null;
-        Connection connection = null;
-
-        Statement stmt = null;
-        try {
-            connection = getConnection();
-            stmt = connection.createStatement();
-        } catch (SQLException e) {
-            logger.warn("Could not create database statement :" + e);
-        } catch (InstantiationException e) {
-            logger.warn("Could not create database statement :" + e);
-        } catch (IllegalAccessException e) {
-            logger.warn("Could not create database statement :" + e);
-        } catch (ClassNotFoundException e) {
-            logger.warn("Could not create database statement :" + e);
-        }
+	}
 
 
 
-        q = "DELETE FROM Workflow";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM Processor";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM Arc";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM Var";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM WfInstance";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM ProcBinding";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM VarBinding";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM Collection";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        q = "DELETE FROM Data";
-        try {
-            stmt.executeUpdate(q);
-        } catch (SQLException e) {
-            logger.warn("Could not execute statement " + q + " :" + e);
-        }
-
-        if (connection!=null) try {
-            connection.close();
-        } catch (SQLException ex) {
-            logger.error("Error closing connection",ex);
-        }
-    }
-
-    /**
-     * The name for this type of provenance connector. Is used by the workbench
-     * to ensure it adds the correct one to the InvocationContext
-     *
-     * @return
-     */
-    public abstract String getName();
-
-    /**
-     * A unique identifier for this run of provenance, should correspond to the
-     * initial {@link WorkflowProvenanceItem} idenifier that gets sent through
-     *
-     * @param identifier
-     */
-    public void setSessionID(String sessionID) {
-        this.sessionID = sessionID;
-    }
-
-    /**
-     * What is the unique identifier used by this connector
-     *
-     * @return
-     */
-    public String getSessionID() {
-        return sessionID;
-    }
-
-    public boolean isClearDB() {
-        return isClearDB;
-    }
-
-    public void setClearDB(boolean isClearDB) {
-        this.isClearDB = isClearDB;
-    }
-
-    /**
-     * Set up the the {@link EventProcessor}, {@link ProvenanceWriter} &
-     * {@link ProvenanceQuery}. Since it is an SPI you don't want any code
-     * cluttering the default constructor. Call this method after instantiation
-     * and after the dbURL has been set.
-     */
-    public abstract void init();
-
-    public List<LineageQueryResultRecord> getIntermediateValues(
-            final String wfInstance, final String pname, final String vname,
-            final String iteration) throws Exception {
-
-        FutureTask<Dependencies> future = new FutureTask<Dependencies>(
-                new Callable<Dependencies>() {
-
-                    public Dependencies call() throws Exception {
-                        try {
-
-                            Dependencies runLineageQuery = getProvenanceAnalysis().fetchIntermediateResult(wfInstance, pname,
-                                    vname, iteration);
-
-                            return runLineageQuery;
-                        } catch (SQLException e) {
-                            throw e;
-                        }
-                    }
-                });
-
-        getExecutor().submit(future);
-
-        try {
-            return future.get().getRecords();
-        } catch (InterruptedException e1) {
-            throw e1;
-        } catch (ExecutionException e1) {
-            throw e1;
-        }
-
-    }
-
-    public List<LineageQueryResultRecord> computeLineage(String wfInstance,
-            String var, String proc, String path, Set<String> selectedProcessors) {
-        return null;
-    }
-
-    public String getDataflowInstance(String dataflowId) {
-        String instanceID = null;
-        try {
-            instanceID = (getProvenance()).getPq().getWFInstanceID(dataflowId).get(0).getInstanceID();
-        } catch (SQLException e) {
-            logger.error("Error finding the dataflow instance", e);
-        }
-        return instanceID;
-    }
-
-    /**
-     * @return the saveEvents
-     */
-    public String getSaveEvents() {
-        return saveEvents;
-    }
-
-    /**
-     * @param saveEvents
-     *            the saveEvents to set
-     */
-    public void setSaveEvents(String saveEvents) {
-        this.saveEvents = saveEvents;
-    }
-
-    public void setProvenance(Provenance provenance) {
-        this.provenance = provenance;
-    }
-
-    public Provenance getProvenance() {
-        return provenance;
-    }
-
-    public void setFinished(boolean finished) {
-        this.finished = finished;
-    }
-
-    public boolean isFinished() {
-        return finished;
-    }
-
-    public void setExecutor(ExecutorService executor) {
-        this.executor = executor;
-    }
-
-    public synchronized ExecutorService getExecutor() {
-        return executor;
-    }
-
-    public void setProvenanceAnalysis(ProvenanceAnalysis provenanceAnalysis) {
-        this.provenanceAnalysis = provenanceAnalysis;
-    }
-
-    /**
-     * Use this {@link ProvenanceAnalysis} to carry out lineage queries on the
-     * provenance
-     *
-     * @return
-     */
-    public ProvenanceAnalysis getProvenanceAnalysis() {
-        return provenanceAnalysis;
-    }
-    
+	/**
+	 * @return the invocationContext
+	 */
 	public InvocationContext getInvocationContext() {
 		return invocationContext;
 	}
 
+	/**
+	 * @param invocationContext the invocationContext to set
+	 */
+	public void setInvocationContext(InvocationContext invocationContext) {
+		this.invocationContext = invocationContext;
+	}
+
+	/**
+	 * @return the referenceService
+	 */
 	public ReferenceService getReferenceService() {
 		return referenceService;
 	}
 
-	public void setInvocationContext(InvocationContext invocationContext) {
-		this.invocationContext = invocationContext;
-		
-	}
-
+	/**
+	 * @param referenceService the referenceService to set
+	 */
 	public void setReferenceService(ReferenceService referenceService) {
 		this.referenceService = referenceService;
-		
+	}
+
+	public ProvenanceConnector(Provenance provenance,
+			ProvenanceAnalysis provenanceAnalysis,
+			boolean isClearDB, String saveEvents) {
+
+		setProvenance(provenance);
+		this.setProvenanceAnalysis(provenanceAnalysis);
+		this.isClearDB = isClearDB;
+		this.saveEvents = saveEvents;
+		getProvenance().setSaveEvents(this.saveEvents);
+	}
+
+	/**
+	 * Uses a {@link ScheduledThreadPoolExecutor} to process events in a Thread
+	 * safe manner
+	 */
+	public synchronized void addProvenanceItem(
+			final ProvenanceItem provenanceItem) {
+
+		Runnable runnable = new Runnable() {
+
+			public void run() {
+				try {
+
+					getProvenance().acceptRawProvenanceEvent(
+							provenanceItem.getEventType(), provenanceItem);
+
+				} catch (SQLException e) {
+					logger.warn("Could not add provenance for " + provenanceItem.getEventType() + " " + provenanceItem.getIdentifier() + " " + e);
+				} catch (IOException e) {
+					logger.warn("Could not add provenance for " + provenanceItem.getEventType() + " " + provenanceItem.getIdentifier() + " " + e);
+				}
+
+			}
+		};
+		getExecutor().execute(runnable);
+
+	}
+
+	protected Connection getConnection() throws InstantiationException,
+	IllegalAccessException, ClassNotFoundException, SQLException {
+		return JDBCConnector.getConnection();
+	}
+
+	/**
+	 * Used by database backed provenance stores. Ask the implementation to
+	 * create the database. Requires each datbase type to create all its own
+	 * tables
+	 */
+	public abstract void createDatabase();    
+
+	/**
+	 * Clear all the values in the database but keep the db there
+	 */
+	public void clearDatabase() {
+		String q = null;
+		Connection connection = null;
+
+		Statement stmt = null;
+		try {
+			connection = getConnection();
+			stmt = connection.createStatement();
+		} catch (SQLException e) {
+			logger.warn("Could not create database statement :" + e);
+		} catch (InstantiationException e) {
+			logger.warn("Could not create database statement :" + e);
+		} catch (IllegalAccessException e) {
+			logger.warn("Could not create database statement :" + e);
+		} catch (ClassNotFoundException e) {
+			logger.warn("Could not create database statement :" + e);
+		}
+
+
+
+		q = "DELETE FROM Workflow";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM Processor";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM Arc";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM Var";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM WfInstance";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM ProcBinding";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM VarBinding";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM Collection";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		q = "DELETE FROM Data";
+		try {
+			stmt.executeUpdate(q);
+		} catch (SQLException e) {
+			logger.warn("Could not execute statement " + q + " :" + e);
+		}
+
+		if (connection!=null) try {
+			connection.close();
+		} catch (SQLException ex) {
+			logger.error("Error closing connection",ex);
+		}
+	}
+
+	/**
+	 * The name for this type of provenance connector. Is used by the workbench
+	 * to ensure it adds the correct one to the InvocationContext
+	 *
+	 * @return
+	 */
+	public abstract String getName();
+
+	/**
+	 * A unique identifier for this run of provenance, should correspond to the
+	 * initial {@link WorkflowProvenanceItem} idenifier that gets sent through
+	 *
+	 * @param identifier
+	 */
+	public void setSessionID(String sessionID) {
+		this.sessionID = sessionID;
+	}
+
+	/**
+	 * What is the unique identifier used by this connector
+	 *
+	 * @return
+	 */
+	public String getSessionID() {
+		return sessionID;
+	}
+
+	public boolean isClearDB() {
+		return isClearDB;
+	}
+
+	public void setClearDB(boolean isClearDB) {
+		this.isClearDB = isClearDB;
+	}
+
+	/**
+	 * Set up the the {@link EventProcessor}, {@link ProvenanceWriter} &
+	 * {@link ProvenanceQuery}. Since it is an SPI you don't want any code
+	 * cluttering the default constructor. Call this method after instantiation
+	 * and after the dbURL has been set.
+	 */
+	public abstract void init();
+
+	public List<LineageQueryResultRecord> getIntermediateValues(
+			final String wfInstance, final String pname, final String vname,
+			final String iteration) throws Exception {
+
+		FutureTask<Dependencies> future = new FutureTask<Dependencies>(
+				new Callable<Dependencies>() {
+
+					public Dependencies call() throws Exception {
+						try {
+
+							Dependencies runLineageQuery = getProvenanceAnalysis().fetchIntermediateResult(wfInstance, pname,
+									vname, iteration);
+
+							return runLineageQuery;
+						} catch (SQLException e) {
+							throw e;
+						}
+					}
+				});
+
+		getExecutor().submit(future);
+
+		try {
+			return future.get().getRecords();
+		} catch (InterruptedException e1) {
+			throw e1;
+		} catch (ExecutionException e1) {
+			throw e1;
+		}
+
+	}
+
+	public List<LineageQueryResultRecord> computeLineage(String wfInstance,
+			String var, String proc, String path, Set<String> selectedProcessors) {
+		return null;
+	}
+
+	public String getDataflowInstance(String dataflowId) {
+		String instanceID = null;
+		try {
+			instanceID = (getProvenance()).getPq().getWFInstanceID(dataflowId).get(0).getInstanceID();
+		} catch (SQLException e) {
+			logger.error("Error finding the dataflow instance", e);
+		}
+		return instanceID;
+	}
+
+	/**
+	 * @return the saveEvents
+	 */
+	 public String getSaveEvents() {
+		return saveEvents;
+	}
+
+	/**
+	 * @param saveEvents
+	 *            the saveEvents to set
+	 */
+	 public void setSaveEvents(String saveEvents) {
+		this.saveEvents = saveEvents;
+	}
+
+	public void setProvenance(Provenance provenance) {
+		this.provenance = provenance;
+	}
+
+	public Provenance getProvenance() {
+		return provenance;
+	}
+
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
+
+	public boolean isFinished() {
+		return finished;
+	}
+
+	public void setExecutor(ExecutorService executor) {
+		this.executor = executor;
+	}
+
+	public synchronized ExecutorService getExecutor() {
+		return executor;
+	}
+
+	public void setProvenanceAnalysis(ProvenanceAnalysis provenanceAnalysis) {
+		this.provenanceAnalysis = provenanceAnalysis;
+	}
+
+	/**
+	 * Use this {@link ProvenanceAnalysis} to carry out lineage queries on the
+	 * provenance
+	 *
+	 * @return
+	 */
+	public ProvenanceAnalysis getProvenanceAnalysis() {
+		return provenanceAnalysis;
 	}
 }
