ef0e1e4580eb82b60d36f328ca5e9c01e2ac2ba8
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2009-11-13 13:54:31 +0000

    Changed println to go to Logger as per T2-578
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@9060 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/serialization/xml/XMLDeserializerRegistry.java
+++ workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/serialization/xml/XMLDeserializerRegistry.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import net.sf.taverna.t2.spi.SPIRegistry;
 
+import org.apache.log4j.Logger;
+
 public class XMLDeserializerRegistry extends SPIRegistry<XMLDeserializer> {
 
+	private static Logger logger = Logger.getLogger(XMLDeserializerRegistry.class);
+	
 	private static XMLDeserializerRegistry instance;
 	
 	protected XMLDeserializerRegistry() {
@@ -23,11 +27,10 @@ public class XMLDeserializerRegistry extends SPIRegistry<XMLDeserializer> {
 		List<XMLDeserializer> instances = getInstance().getInstances();
 		XMLDeserializer result = null;
 		if (instances.size() == 0) {
-			System.out.println("No Deserializer implementation defined");
+			logger.error("No Deserializer implementation defined");
 		} else {
 			if (instances.size() > 1)
-				System.out
-						.println("More that 1 XML Deserializer implementation defined, using the first");
+				logger.error("More that 1 XML Deserializer implementation defined, using the first");
 			result=instances.get(0);
 		}
 		return result;
