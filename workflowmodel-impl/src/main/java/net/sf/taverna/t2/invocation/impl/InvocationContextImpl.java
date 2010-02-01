74b41e706a174d685671259447a701e801be667d
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-02-01 16:03:03 +0000

    T2-1124 tidied up thread safe code
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@9652 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-impl/src/main/java/net/sf/taverna/t2/invocation/impl/InvocationContextImpl.java
+++ workflowmodel-impl/src/main/java/net/sf/taverna/t2/invocation/impl/InvocationContextImpl.java
@@ -29,18 +29,18 @@ import net.sf.taverna.t2.provenance.reporter.ProvenanceReporter;
 import net.sf.taverna.t2.reference.ReferenceService;
 
 public class InvocationContextImpl implements InvocationContext {
-	
+
 	private final ReferenceService referenceService;
 
 	private final ProvenanceReporter provenanceReporter;
 
-	private List<Object> entities;
-	
+	private List<Object> entities = Collections
+			.synchronizedList(new ArrayList<Object>());
+
 	public InvocationContextImpl(ReferenceService referenceService,
 			ProvenanceReporter provenanceReporter) {
 		this.referenceService = referenceService;
 		this.provenanceReporter = provenanceReporter;
-		entities = Collections.synchronizedList(new ArrayList<Object>());
 	}
 
 	public ReferenceService getReferenceService() {
@@ -51,23 +51,23 @@ public class InvocationContextImpl implements InvocationContext {
 		return provenanceReporter;
 	}
 
-	public <T extends Object> List<T> getEntities(Class<T> entityType){
+	public <T extends Object> List<T> getEntities(Class<T> entityType) {
 		List<T> entitiesOfType = new ArrayList<T>();
-		synchronized(entities) {
-			for (Object entity : entities){
-				if (entityType.isInstance(entity)){
+		synchronized (entities) {
+			for (Object entity : entities) {
+				if (entityType.isInstance(entity)) {
 					entitiesOfType.add(entityType.cast(entity));
 				}
 			}
 		}
 		return entitiesOfType;
 	}
-	
-	public void addEntity(Object entity){
+
+	public void addEntity(Object entity) {
 		entities.add(entity);
 	}
-	
-	public void removeEntity(Object entity){
+
+	public void removeEntity(Object entity) {
 		entities.remove(entity);
 	}
 }
