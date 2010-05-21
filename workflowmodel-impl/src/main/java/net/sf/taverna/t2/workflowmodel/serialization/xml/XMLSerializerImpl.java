67e86ce0336fa1f4106f92abc325fc265d9be9ba
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-05-21 14:04:46 +0000

    Fixed spelling mistake
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@10480 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-impl/src/main/java/net/sf/taverna/t2/workflowmodel/serialization/xml/XMLSerializerImpl.java
+++ workflowmodel-impl/src/main/java/net/sf/taverna/t2/workflowmodel/serialization/xml/XMLSerializerImpl.java
@@ -64,7 +64,7 @@ public class XMLSerializerImpl implements XMLSerializer, XMLSerializationConstan
 		
 		Set<String> outputIds = new HashSet<String>();
 		for (Dataflow innerDataflow : innerDataflows) {
-			String currentId = innerDataflow.getInternalIdentier();
+			String currentId = innerDataflow.getInternalIdentifier();
 			if (outputIds.add(currentId)) {
 				Element innerDataflowElement = DataflowXMLSerializer.getInstance().serializeDataflow(innerDataflow);
 				innerDataflowElement.setAttribute(DATAFLOW_ROLE,DATAFLOW_ROLE_NESTED);
