1bc49c95914d32bbb5c5ed8227fc3d36ff8b96cc
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-05-21 14:39:42 +0000

    Modified health checkers to cope with DisabledActivity and new VisitReport as per T2-1238 and T2-1231
    git-svn-id: http://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.activities/trunk/dataflow-activity@10490 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/dataflow/DataflowActivityHealthChecker.java
+++ src/main/java/net/sf/taverna/t2/activities/dataflow/DataflowActivityHealthChecker.java
@@ -25,40 +25,27 @@ import java.util.List;
 
 import net.sf.taverna.t2.workflowmodel.Dataflow;
 import net.sf.taverna.t2.workflowmodel.Processor;
+import net.sf.taverna.t2.workflowmodel.health.HealthCheck;
 import net.sf.taverna.t2.workflowmodel.health.HealthChecker;
-import net.sf.taverna.t2.workflowmodel.health.HealthCheckerFactory;
-import net.sf.taverna.t2.workflowmodel.health.HealthReport;
-import net.sf.taverna.t2.workflowmodel.health.HealthReport.Status;
+import net.sf.taverna.t2.visit.VisitReport;
+import net.sf.taverna.t2.visit.VisitReport.Status;
+
+import net.sf.taverna.t2.visit.VisitReport;
+import net.sf.taverna.t2.visit.VisitReport.Status;
 
 public class DataflowActivityHealthChecker implements HealthChecker<DataflowActivity> {
 
-	public boolean canHandle(Object subject) {
-		return subject!=null && subject instanceof DataflowActivity;
+	public boolean canVisit(Object subject) {
+		return false;
 	}
 
 	@SuppressWarnings("unchecked")
-	public HealthReport checkHealth(DataflowActivity activity) {
-		Dataflow dataflow = activity.getConfiguration();
-		Status status = Status.OK;
-		String message = "Everything seems fine";
-		List<HealthReport> subReports = new ArrayList<HealthReport>();
-		for (Processor processor : dataflow.getProcessors()) {
-			for (HealthChecker checker : HealthCheckerFactory.getInstance().getHealthCheckersForObject(processor)) {
-				HealthReport subReport = checker.checkHealth(processor);
-				if (subReport.getStatus().equals(Status.WARNING)) {
-					if (status.equals(Status.OK)) {
-						status = Status.WARNING;
-						message = "Some warnings reported";
-					}
-				} else if (subReport.getStatus().equals(Status.SEVERE)) {
-					status = Status.SEVERE;
-					message = "We have a problem";
-				}
-				subReports.add(subReport);
-			}
-			
-		}
-		return new HealthReport("Workflow Activity", message, status, subReports);
+	public VisitReport visit(DataflowActivity activity, List<Object> ancestors) {
+		return null;
+	}
+
+	public boolean isTimeConsuming() {
+		return true;
 	}
 
 }
