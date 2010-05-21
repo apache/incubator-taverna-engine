11c51660707ad1426955925a93a1fa2905c10545
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-05-21 14:07:08 +0000

    Added creation of DisabledActivity if an activity cannot be configured, as per T2-1231
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@10482 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-impl/src/main/java/net/sf/taverna/t2/workflowmodel/serialization/xml/ActivityXMLDeserializer.java
+++ workflowmodel-impl/src/main/java/net/sf/taverna/t2/workflowmodel/serialization/xml/ActivityXMLDeserializer.java
@@ -24,8 +24,11 @@ import java.util.List;
 import java.util.Map;
 
 import net.sf.taverna.t2.workflowmodel.EditException;
+import net.sf.taverna.t2.workflowmodel.ProcessorInputPort;
 import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityAndBeanWrapper;
 import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityConfigurationException;
+import net.sf.taverna.t2.workflowmodel.processor.activity.DisabledActivity;
 import net.sf.taverna.t2.workflowmodel.serialization.DeserializationException;
 
 import org.apache.log4j.Logger;
@@ -66,7 +69,7 @@ public class ActivityXMLDeserializer extends AbstractXMLDeserializer {
 				.getTextTrim();
 		Class<? extends Activity> c = (Class<? extends Activity>) cl
 				.loadClass(className);
-		Activity<Object> activity = c.newInstance();
+		Activity activity = c.newInstance();
 		
 		// Handle the configuration of the activity
 		Element configElement = element.getChild(CONFIG_BEAN,
@@ -79,7 +82,11 @@ public class ActivityXMLDeserializer extends AbstractXMLDeserializer {
 		else {
 			configObject = createBean(configElement, cl);
 		}
-		activity.configure(configObject);
+		try {
+			activity.configure(configObject);
+		} catch (ActivityConfigurationException e) {
+			activity = new DisabledActivity(c, configObject);
+		}
 
 		//port mappings
 		Element ipElement = element.getChild(INPUT_MAP, T2_WORKFLOW_NAMESPACE);
@@ -87,6 +94,18 @@ public class ActivityXMLDeserializer extends AbstractXMLDeserializer {
 				T2_WORKFLOW_NAMESPACE))) {
 			String processorInputName = mapElement.getAttributeValue(FROM);
 			String activityInputName = mapElement.getAttributeValue(TO);
+			if (activity instanceof DisabledActivity) {
+				Element processorElement = element.getParentElement().getParentElement();
+					Element inputPorts = processorElement.getChild(PROCESSOR_INPUT_PORTS,T2_WORKFLOW_NAMESPACE);
+					int depth = 0;
+					for (Element inputPort : (List<Element>)inputPorts.getChildren(PROCESSOR_PORT,T2_WORKFLOW_NAMESPACE)) {
+							String portName=inputPort.getChildText(NAME,T2_WORKFLOW_NAMESPACE);
+							if (portName.equals(processorInputName)) {
+								depth = Integer.valueOf(inputPort.getChildText(DEPTH,T2_WORKFLOW_NAMESPACE));
+							}
+						}
+				((DisabledActivity) activity).addProxyInput(activityInputName, depth);
+			}
 			activity.getInputPortMapping().put(processorInputName,
 					activityInputName);
 		}
@@ -96,6 +115,18 @@ public class ActivityXMLDeserializer extends AbstractXMLDeserializer {
 				T2_WORKFLOW_NAMESPACE))) {
 			String activityOutputName = mapElement.getAttributeValue(FROM);
 			String processorOutputName = mapElement.getAttributeValue(TO);
+			if (activity instanceof DisabledActivity) {
+				Element processorElement = element.getParentElement().getParentElement();
+				int depth = 0;
+				Element outputPorts = processorElement.getChild(PROCESSOR_OUTPUT_PORTS,T2_WORKFLOW_NAMESPACE);
+				for (Element outputPort : (List<Element>)outputPorts.getChildren(PROCESSOR_PORT,T2_WORKFLOW_NAMESPACE)) {
+					String portName=outputPort.getChildText(NAME,T2_WORKFLOW_NAMESPACE);
+					if (portName.equals(processorOutputName)) {
+						depth = Integer.valueOf(outputPort.getChildText(DEPTH,T2_WORKFLOW_NAMESPACE));
+					}
+				}
+				((DisabledActivity) activity).addProxyOutput(activityOutputName, depth);
+			}
 			activity.getOutputPortMapping().put(activityOutputName,
 					processorOutputName);
 		}
