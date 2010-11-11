7b24600e80bad37537b2a43703ab1eef47d13da6
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2010-11-11 14:41:35 +0000

    Updated integration tests
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/platform/trunk/platform@11922 bf327186-88b3-11dd-a302-d386e5130c1c

--- integration-tests/src/test/java/uk/org/taverna/platform/LocalExecutionTest.java
+++ integration-tests/src/test/java/uk/org/taverna/platform/LocalExecutionTest.java
@@ -2,9 +2,14 @@ package uk.org.taverna.platform;
 
 import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import net.sf.taverna.t2.reference.ErrorDocument;
+import net.sf.taverna.t2.reference.IdentifiedList;
 import net.sf.taverna.t2.reference.ReferenceService;
+import net.sf.taverna.t2.reference.StackTraceElementBean;
 import net.sf.taverna.t2.reference.T2Reference;
 
 import org.osgi.framework.ServiceReference;
@@ -19,7 +24,7 @@ import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
 
 public class LocalExecutionTest extends PlatformTest {
 
-	public void testDataflowExecution() throws Exception {
+	public void testLocalExecution() throws Exception {
 		URL wfResource = getClass().getResource("/t2flow/in-out.t2flow");
 		assertNotNull(wfResource);
 		WorkflowBundle researchObject = new T2FlowParser().parseT2Flow(wfResource.openStream());
@@ -30,36 +35,106 @@ public class LocalExecutionTest extends PlatformTest {
 				.getServiceReference("net.sf.taverna.t2.reference.ReferenceService");
 		ReferenceService referenceService = (ReferenceService) bundleContext
 				.getService(referenceServiceReference);
-		
+
 		ServiceReference executionServiceReference = bundleContext
 				.getServiceReference("uk.org.taverna.platform.execution.api.ExecutionService");
-		ExecutionService executionService = (ExecutionService) bundleContext.getService(executionServiceReference);
-		
+		ExecutionService executionService = (ExecutionService) bundleContext
+				.getService(executionServiceReference);
+
 		T2Reference reference = referenceService.register("test-input", 0, true, null);
 
 		Map<String, T2Reference> inputs = new HashMap<String, T2Reference>();
 		inputs.put("in", reference);
 
-		String executionId = executionService.createExecution(workflow, profile, inputs, referenceService);
+		String executionId = executionService.createExecution(workflow, profile, inputs,
+				referenceService);
 		WorkflowReport report = executionService.getWorkflowReport(executionId);
 		assertEquals(State.CREATED, report.getState());
 		executionService.start(executionId);
 
 		Map<String, T2Reference> results = report.getOutputs();
+		assertNotNull(results);
 		waitForResult(results, "out", report);
 
-		String result = (String) referenceService.renderIdentifier(results.get("out"), String.class, null);
+		String result = (String) referenceService.renderIdentifier(results.get("out"),
+				String.class, null);
 		assertEquals("test-input", result);
 		assertEquals(State.COMPLETED, report.getState());
 		System.out.println(report);
 	}
 
-	private void waitForResult(Map<String, T2Reference> results, String port, WorkflowReport report)
-			throws InterruptedException {
-		int wait = 0;
-		while (!results.containsKey(port) && wait++ < 10) {
-			System.out.println(report);
-			Thread.sleep(500);
+	public void testLocalExecution2() throws Exception {
+		URL wfResource = getClass().getResource("/t2flow/beanshell.t2flow");
+		assertNotNull(wfResource);
+		T2FlowParser t2FlowParser = new T2FlowParser();
+		t2FlowParser.setStrict(true);
+		WorkflowBundle researchObject = t2FlowParser.parseT2Flow(wfResource.openStream());
+		Workflow workflow = researchObject.getMainWorkflow();
+		Profile profile = researchObject.getProfiles().iterator().next();
+
+		ServiceReference referenceServiceReference = bundleContext
+				.getServiceReference("net.sf.taverna.t2.reference.ReferenceService");
+		ReferenceService referenceService = (ReferenceService) bundleContext
+				.getService(referenceServiceReference);
+
+		ServiceReference executionServiceReference = bundleContext
+				.getServiceReference("uk.org.taverna.platform.execution.api.ExecutionService");
+		ExecutionService executionService = (ExecutionService) bundleContext
+				.getService(executionServiceReference);
+
+		T2Reference reference = referenceService.register("test-input", 0, true, null);
+		Map<String, T2Reference> inputs = new HashMap<String, T2Reference>();
+		inputs.put("in", reference);
+
+		String executionId = executionService.createExecution(workflow, profile, inputs,
+				referenceService);
+		WorkflowReport report = executionService.getWorkflowReport(executionId);
+		System.out.println(report);
+		assertEquals(State.CREATED, report.getState());
+		executionService.start(executionId);
+		System.out.println(report);
+
+		Map<String, T2Reference> results = report.getOutputs();
+		waitForResult(results, "out", report);
+
+		T2Reference resultReference = results.get("out");
+		if (resultReference.containsErrors()) {
+			printErrors(referenceService, resultReference);
+		}
+		assertFalse(resultReference.containsErrors());
+		List<String> result = (List<String>) referenceService.renderIdentifier(
+				(T2Reference) results.get("out"), String.class, null);
+		assertEquals(1000, result.size());
+		assertEquals("test-input:0", result.get(0));
+		assertEquals(State.COMPLETED, report.getState());
+		System.out.println(report);
+	}
+
+	private void printErrors(ReferenceService referenceService, T2Reference resultReference) {
+		if (resultReference.getDepth() > 0) {
+			IdentifiedList<T2Reference> list = referenceService.getListService().getList(
+					resultReference);
+			for (T2Reference t2Reference : list) {
+				printErrors(referenceService, t2Reference);
+			}
+		} else if (resultReference.containsErrors()) {
+			ErrorDocument error = referenceService.getErrorDocumentService().getError(
+					resultReference);
+			String message = error.getMessage();
+			if (message != null) {
+				System.out.println(message);
+			}
+			String exceptionMessage = error.getExceptionMessage();
+			if (exceptionMessage != null) {
+				System.out.println(exceptionMessage);
+			}
+			for (StackTraceElementBean stackTraceElementBean : error.getStackTraceStrings()) {
+				System.out.println(stackTraceElementBean);
+			}
+			Set<T2Reference> errorReferences = error.getErrorReferences();
+			for (T2Reference t2Reference : errorReferences) {
+				printErrors(referenceService, t2Reference);
+			}
 		}
 	}
 
---
+++ integration-tests/src/test/java/uk/org/taverna/platform/RunTest.java
@@ -0,0 +1,115 @@
+package uk.org.taverna.platform;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.taverna.t2.reference.ReferenceService;
+import net.sf.taverna.t2.reference.T2Reference;
+
+import org.osgi.framework.ServiceReference;
+
+import uk.org.taverna.platform.execution.api.ExecutionService;
+import uk.org.taverna.platform.report.State;
+import uk.org.taverna.platform.report.WorkflowReport;
+import uk.org.taverna.platform.run.api.RunProfile;
+import uk.org.taverna.platform.run.api.RunService;
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.profiles.Profile;
+import uk.org.taverna.scufl2.translator.t2flow.T2FlowParser;
+
+public class RunTest extends PlatformTest {
+
+	private ReferenceService referenceService;
+	private ExecutionService executionService;
+	private RunService runService;
+
+	public void testRun() throws Exception {
+		setup();
+
+		WorkflowBundle workflowBundle = loadWorkflow("/t2flow/in-out.t2flow");
+		Profile profile = workflowBundle.getProfiles().iterator().next();
+
+		T2Reference reference = referenceService.register("test-input", 0, true, null);
+
+		Map<String, T2Reference> inputs = new HashMap<String, T2Reference>();
+		inputs.put("in", reference);
+
+		String runId = runService.createRun(new RunProfile(workflowBundle, profile, inputs, referenceService,
+				executionService));
+		assertEquals(State.CREATED, runService.getState(runId));
+		runService.start(runId);
+
+		Map<String, T2Reference> results = runService.getOutputs(runId);
+		assertNotNull(results);
+		waitForResult(results, "out",  runService.getWorkflowReport(runId));
+
+		String result = (String) referenceService.renderIdentifier(results.get("out"),
+				String.class, null);
+		assertEquals("test-input", result);
+		assertEquals(State.COMPLETED, runService.getState(runId));
+		System.out.println( runService.getWorkflowReport(runId));
+	}
+
+	public void testRun2() throws Exception {
+		setup();
+
+		WorkflowBundle workflowBundle = loadWorkflow("/t2flow/beanshell.t2flow");
+		Profile profile = workflowBundle.getProfiles().iterator().next();
+
+		T2Reference reference = referenceService.register("test-input", 0, true, null);
+		Map<String, T2Reference> inputs = new HashMap<String, T2Reference>();
+		inputs.put("in", reference);
+
+		String runId = runService.createRun(new RunProfile(workflowBundle, profile, inputs, referenceService,
+				executionService));
+		WorkflowReport report = runService.getWorkflowReport(runId);
+		assertEquals(State.CREATED, runService.getState(runId));
+		System.out.println(report);
+
+		runService.start(runId);
+		assertEquals(State.RUNNING, runService.getState(runId));
+		System.out.println(report);
+
+		Map<String, T2Reference> results = runService.getOutputs(runId);
+		waitForResult(results, "out", report);
+
+		T2Reference resultReference = results.get("out");
+		// if (resultReference.containsErrors()) {
+		// printErrors(referenceService, resultReference);
+		// }
+		assertFalse(resultReference.containsErrors());
+		List<String> result = (List<String>) referenceService.renderIdentifier(
+				(T2Reference) results.get("out"), String.class, null);
+		assertEquals(1000, result.size());
+		assertEquals("test-input:0", result.get(0));
+		assertEquals(State.COMPLETED, runService.getState(runId));
+		System.out.println(report);
+	}
+
+	private void setup() {
+		ServiceReference referenceServiceReference = bundleContext
+				.getServiceReference("net.sf.taverna.t2.reference.ReferenceService");
+		referenceService = (ReferenceService) bundleContext
+				.getService(referenceServiceReference);
+
+		ServiceReference executionServiceReference = bundleContext
+				.getServiceReference("uk.org.taverna.platform.execution.api.ExecutionService");
+		executionService = (ExecutionService) bundleContext
+				.getService(executionServiceReference);
+
+		ServiceReference runServiceReference = bundleContext
+				.getServiceReference("uk.org.taverna.platform.run.api.RunService");
+		runService = (RunService) bundleContext.getService(runServiceReference);
+	}
+	
+	private WorkflowBundle loadWorkflow(String t2FlowFile) throws Exception {
+		URL wfResource = getClass().getResource(t2FlowFile);
+		assertNotNull(wfResource);
+		T2FlowParser t2FlowParser = new T2FlowParser();
+		t2FlowParser.setStrict(true);
+		return t2FlowParser.parseT2Flow(wfResource.openStream());
+	}
+
+}
