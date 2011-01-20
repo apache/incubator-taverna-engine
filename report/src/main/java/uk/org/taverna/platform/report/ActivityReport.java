63abae2d673a84ff4203d8366ee73847398453ef
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-01-20 12:53:36 +0000

    Renamed artifact 'monitor' to 'report'
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/platform/trunk/platform@12228 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ report/src/main/java/uk/org/taverna/platform/report/ActivityReport.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (C) 2010 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package uk.org.taverna.platform.report;
+
+import uk.org.taverna.scufl2.api.activity.Activity;
+
+/**
+ * 
+ * @author David Withers
+ */
+public class ActivityReport extends StatusReport {
+
+	private final Activity activity;
+	
+	private final ProcessorReport parentReport;
+	
+	private final WorkflowReport nestedWorkflowReport;
+		
+	public ActivityReport(Activity activity, ProcessorReport parentReport) {
+		this(activity, parentReport, null);
+	}
+
+	public ActivityReport(Activity activity, ProcessorReport parentReport, WorkflowReport nestedWorkflowReport) {
+		this.activity = activity;
+		this.parentReport = parentReport;
+		this.nestedWorkflowReport = nestedWorkflowReport;
+	}
+
+	/**
+	 * @return the activity
+	 */
+	public Activity getActivity() {
+		return activity;
+	}
+
+	/**
+	 * @return the parentReport
+	 */
+	public ProcessorReport getParentReport() {
+		return parentReport;
+	}
+
+	public WorkflowReport getNestedWorkflowReport() {
+		return nestedWorkflowReport;
+	}
+
+}
---
+++ report/src/main/java/uk/org/taverna/platform/report/StatusReport.java
@@ -0,0 +1,228 @@
+/*******************************************************************************
+ * Copyright (C) 2010 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package uk.org.taverna.platform.report;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 
+ * @author David Withers
+ */
+public class StatusReport {
+
+	private State state;
+
+	private Date createdDate, startedDate, pausedDate, resumedDate,
+			cancelledDate, completedDate, failedDate;
+
+	private List<Date> pausedDates, resumedDates;
+
+	public StatusReport() {
+		pausedDates = new ArrayList<Date>();
+		resumedDates = new ArrayList<Date>();
+		setCreatedDate(new Date());
+	}
+
+	/**
+	 * Returns the current state. A state can be CREATED, RUNNING, COMPLETED,
+	 * PAUSED, CANCELLED or FAILED.
+	 * 
+	 * @return the current state
+	 */
+	public State getState() {
+		return state;
+	}
+
+	/**
+	 * Returns the date that the status was set to CREATED.
+	 * 
+	 * @return the the date that the status was set to CREATED
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	/**
+	 * Sets the date that the status was set to CREATED.
+	 * 
+	 * @param createdDate
+	 *            the date that the status was set to CREATED
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+		state = State.CREATED;
+	}
+
+	/**
+	 * Returns the date that the status changed to RUNNING. If the status has
+	 * never been RUNNING <code>null</code> is returned.
+	 * 
+	 * @return the date that the status changed to started
+	 */
+	public Date getStartedDate() {
+		return startedDate;
+	}
+
+	/**
+	 * Sets the date that the status changed to RUNNING.
+	 * 
+	 * @param startedDate
+	 *            the date that the status changed to RUNNING
+	 */
+	public void setStartedDate(Date startedDate) {
+		if (this.startedDate == null) {
+			this.startedDate = startedDate;
+		}
+		state = State.RUNNING;
+	}
+
+	/**
+	 * Returns the date that the status last changed to PAUSED. If the status
+	 * has never been PAUSED <code>null</code> is returned.
+	 * 
+	 * @return the date that the status last changed to PAUSED
+	 */
+	public Date getPausedDate() {
+		return pausedDate;
+	}
+
+	/**
+	 * Sets the date that the status last changed to PAUSED.
+	 * 
+	 * @param pausedDate
+	 *            the date that the status last changed to PAUSED
+	 */
+	public void setPausedDate(Date pausedDate) {
+		this.pausedDate = pausedDate;
+		pausedDates.add(pausedDate);
+		state = State.PAUSED;
+	}
+
+	/**
+	 * Returns the date that the status last changed form PAUSED to RUNNING. If
+	 * the status has never changed form PAUSED to RUNNING <code>null</code> is
+	 * returned.
+	 * 
+	 * @return the date that the status last changed form PAUSED to RUNNING
+	 */
+	public Date getResumedDate() {
+		return resumedDate;
+	}
+
+	/**
+	 * Sets the date that the status last changed form PAUSED to RUNNING.
+	 * 
+	 * @param resumedDate
+	 *            the date that the status last changed form PAUSED to RUNNING
+	 */
+	public void setResumedDate(Date resumedDate) {
+		this.resumedDate = resumedDate;
+		resumedDates.add(resumedDate);
+		state = State.RUNNING;
+	}
+
+	/**
+	 * Returns the date that the status changed to CANCELLED. If the status has
+	 * never been CANCELLED <code>null</code> is returned.
+	 * 
+	 * @return the date that the status changed to canceled
+	 */
+	public Date getCancelledDate() {
+		return cancelledDate;
+	}
+
+	/**
+	 * Sets the date that the status changed to CANCELLED.
+	 * 
+	 * @param cancelledDate
+	 *            the date that the status changed to CANCELLED
+	 */
+	public void setCancelledDate(Date cancelledDate) {
+		this.cancelledDate = cancelledDate;
+		state = State.CANCELLED;
+	}
+
+	/**
+	 * Returns the date that the status changed to COMPLETED. If the status
+	 * never been COMPLETED <code>null</code> is returned.
+	 * 
+	 * @return the date that the status changed to COMPLETED
+	 */
+	public Date getCompletedDate() {
+		return completedDate;
+	}
+
+	/**
+	 * Sets the date that the status changed to COMPLETED.
+	 * 
+	 * @param completedDate
+	 *            the date that the status changed to COMPLETED
+	 */
+	public void setCompletedDate(Date completedDate) {
+		this.completedDate = completedDate;
+		state = State.COMPLETED;
+	}
+
+	/**
+	 * Returns the date that the status changed to FAILED. If the status has
+	 * never been FAILED <code>null</code> is returned.
+	 * 
+	 * @return the date that the status changed to failed
+	 */
+	public Date getFailedDate() {
+		return failedDate;
+	}
+
+	/**
+	 * Sets the date that the status changed to FAILED.
+	 * 
+	 * @param failedDate
+	 *            the date that the status changed to FAILED
+	 */
+	public void setFailedDate(Date failedDate) {
+		this.failedDate = failedDate;
+		state = State.FAILED;
+	}
+
+	/**
+	 * Returns the dates that the status changed to PAUSED. If the status has
+	 * never been PAUSED an empty list is returned.
+	 * 
+	 * @return the dates that the status was paused
+	 */
+	public List<Date> getPausedDates() {
+		return pausedDates;
+	}
+
+	/**
+	 * Returns the dates that the status changed from PAUSED to RUNNING. If the
+	 * status has never changed from PAUSED to RUNNING an empty list is
+	 * returned.
+	 * 
+	 * @return the dates that the status was resumed
+	 */
+	public List<Date> getResumedDates() {
+		return resumedDates;
+	}
+
+}
