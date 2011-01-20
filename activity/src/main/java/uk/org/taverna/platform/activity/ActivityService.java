3f896c1fb0e19a469b4125a8ced29292254fb828
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-01-20 13:21:57 +0000

    Added documentation.
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/platform/trunk/platform@12232 bf327186-88b3-11dd-a302-d386e5130c1c

--- activity/src/main/java/uk/org/taverna/platform/activity/ActivityService.java
+++ activity/src/main/java/uk/org/taverna/platform/activity/ActivityService.java
@@ -61,17 +61,47 @@ public interface ActivityService {
 	 * @return a definition of the configuration required by an activity
 	 * @throws ActivityNotFoundException
 	 *             if an activity cannot be found for the specified URI
-	 * @throws ActivityConfigurationException 
+	 * @throws ActivityConfigurationException
+	 *             if the ConfigurationDefinition cannot be created
 	 */
 	public ConfigurationDefinition getActivityConfigurationDefinition(URI uri)
 			throws ActivityNotFoundException, ActivityConfigurationException;
 
+	/**
+	 * Adds ports to the activity that would be created by configuring the activity with the
+	 * configuration.
+	 * 
+	 * A configuration may not create any dynamic ports for the activity.
+	 * 
+	 * @param activity
+	 *            the activity to add ports to
+	 * @param configuration
+	 *            the configuration for the activity
+	 * @throws ActivityNotFoundException
+	 *             if a configurable version of the activity cannot be found
+	 * @throws ActivityConfigurationException
+	 *             if the activity cannot be configured
+	 */
 	public void addDynamicPorts(Activity activity, Configuration configuration)
 			throws ActivityNotFoundException, ActivityConfigurationException;
 
+	/**
+	 * Returns the activity for the specified URI.
+	 * 
+	 * If configuration is not null the returned activity will be configured.
+	 * 
+	 * @param uri
+	 *            a URI that identifies an activity
+	 * @param configuration
+	 *            the configuration for the activity, can be <code>null</code>
+	 * @return the activity for the specified URI
+	 * @throws ActivityNotFoundException
+	 *             if an activity cannot be found for the specified URI
+	 * @throws ActivityConfigurationException
+	 *             if the configuration is not valid
+	 */
 	public net.sf.taverna.t2.workflowmodel.processor.activity.Activity<?> createActivity(URI uri,
-			Configuration configuration/*, Set<InputActivityPort> inputs,
-			Set<OutputActivityPort> outputs*/) throws ActivityNotFoundException,
+			Configuration configuration) throws ActivityNotFoundException,
 			ActivityConfigurationException;
 
 	/**
