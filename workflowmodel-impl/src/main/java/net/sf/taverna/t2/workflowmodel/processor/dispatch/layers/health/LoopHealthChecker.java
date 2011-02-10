24bd785f818ea3562d33a98dd1cce9489ceb0b08
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-10 13:31:53 +0000

    Added health checker for loop connection problems as per T2-641
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/branches/maintenance@12523 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ workflowmodel-impl/src/main/java/net/sf/taverna/t2/workflowmodel/processor/dispatch/layers/health/LoopHealthChecker.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.workflowmodel.processor.dispatch.layers.health;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.taverna.t2.visit.VisitReport;
+import net.sf.taverna.t2.visit.VisitReport.Status;
+import net.sf.taverna.t2.workflowmodel.OutputPort;
+import net.sf.taverna.t2.workflowmodel.Processor;
+import net.sf.taverna.t2.workflowmodel.health.HealthCheck;
+import net.sf.taverna.t2.workflowmodel.health.HealthChecker;
+import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
+import net.sf.taverna.t2.workflowmodel.processor.dispatch.DispatchLayer;
+import net.sf.taverna.t2.workflowmodel.processor.dispatch.DispatchStack;
+import net.sf.taverna.t2.workflowmodel.processor.dispatch.layers.Loop;
+
+/**
+ * @author alanrw
+ *
+ */
+public class LoopHealthChecker implements HealthChecker<Processor> {
+
+	public boolean canVisit(Object o) {
+		return ((o != null) && (o instanceof Processor));
+	}
+
+	public boolean isTimeConsuming() {
+		return false;
+	}
+
+	public VisitReport visit(Processor o, List<Object> ancestry) {
+		List<VisitReport> reports = new ArrayList<VisitReport>();
+		DispatchStack ds = o.getDispatchStack();
+		for (DispatchLayer<?> dl : ds.getLayers()) {
+			if (dl instanceof Loop) {
+				Activity<?> conditionActivity = ((Loop)dl).getConfiguration().getCondition();
+				for (OutputPort aop : conditionActivity.getOutputPorts()) {
+					String portName = aop.getName();
+					
+					// The loop port is used to control whether the main activity should loop
+					if (portName.equals("loop")) {
+						continue;
+					}
+					boolean found = false;
+					for (OutputPort pop : o.getOutputPorts()) {
+						System.err.println("Comparing " + pop.getName() + " with " + portName);
+						if (pop.getName().equals(portName)) {
+							found = true;
+							break;
+						}
+					}
+					if (!found) {
+						VisitReport vr = new VisitReport(HealthCheck.getInstance(), o, "Missing connection", HealthCheck.LOOP_CONNECTION, Status.SEVERE);
+						vr.setProperty("portname", portName);
+						reports.add(vr);
+					}
+				}
+			}
+		}
+		if (reports.isEmpty()) {
+			return null;
+		} else {
+			Status status = VisitReport.getWorstStatus(reports);
+			return new VisitReport(HealthCheck.getInstance(), o, "Loop layer report", HealthCheck.NO_PROBLEM,
+					status, reports);
+		}
+	}
+
+}
