94fe973843b2d0f0e9897185618974aa63a2fcc1
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-15 10:40:10 +0000

    Added module for remote execution
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/platform/trunk/platform@12568 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ execution-remote/src/main/java/uk/org/taverna/platform/execution/impl/remote/RemoteExecution.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (C) 2011 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package uk.org.taverna.platform.execution.impl.remote;
+
+import java.util.Map;
+
+import net.sf.taverna.t2.reference.ReferenceService;
+import net.sf.taverna.t2.reference.T2Reference;
+import uk.org.taverna.platform.execution.api.AbstractExecution;
+import uk.org.taverna.platform.report.WorkflowReport;
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.Workflow;
+import uk.org.taverna.scufl2.api.profiles.Profile;
+
+/**
+ * An {@link uk.org.taverna.platform.execution.api.Execution Execution} for executing a Taverna workflow on a Taverna Server.
+ * 
+ * @author David Withers
+ */
+public class RemoteExecution extends AbstractExecution {
+
+	public RemoteExecution(WorkflowBundle workflowBundle, Workflow workflow, Profile profile,
+			Map<String, T2Reference> inputs, ReferenceService referenceService) {
+		super(workflowBundle, workflow, profile, inputs, referenceService);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected WorkflowReport createWorkflowReport(Workflow workflow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
