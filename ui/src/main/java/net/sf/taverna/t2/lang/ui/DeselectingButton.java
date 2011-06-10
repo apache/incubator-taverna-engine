c328c2cd4957eca72a782b297061c8739b5b172c
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-06-10 16:13:37 +0000

    Button that deselects after action
    git-svn-id: https://taverna.googlecode.com/svn/taverna/utils/net.sf.taverna.t2.lang/branches/maintenance@13253 bf327186-88b3-11dd-a302-d386e5130c1c

--- ui/src/main/java/net/sf/taverna/t2/lang/ui/DeselectingButton.java
+++ ui/src/main/java/net/sf/taverna/t2/lang/ui/DeselectingButton.java
@@ -5,8 +5,10 @@ package net.sf.taverna.t2.lang.ui;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 
 /**
@@ -15,7 +17,7 @@ import javax.swing.JButton;
  */
 public class DeselectingButton extends JButton {
 	
-	public DeselectingButton(String name, final AbstractAction action) {
+	public DeselectingButton(String name, final ActionListener action, String toolTip) {
 		super();
 		this.setAction(new AbstractAction() {
 
@@ -26,6 +28,18 @@ public class DeselectingButton extends JButton {
 			}		
 		});
 		this.setText(name);
+		this.setToolTipText(toolTip);
 	}
 
+	public DeselectingButton(String name, final ActionListener action) {
+		this(name, action, null);
+	}
+	
+	public DeselectingButton(final Action action, String toolTip) {
+		this((String) action.getValue(Action.NAME), action, toolTip);
+	}
+
+	public DeselectingButton(final Action action) {
+		this(action, null);
+	}
 }
