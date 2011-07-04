d8a11ce735ffca55b41f8b0dbb819137bae35c0a
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-07-04 10:57:39 +0000

    1.3 branch
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core.reference-core-extensions/branches/1.3@13413 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ maintenance/src/main/java/net/sf/taverna/t2/reference/impl/external/object/StreamToBooleanConverter.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.reference.impl.external.object;
+
+import java.io.InputStream;
+
+import net.sf.taverna.t2.reference.ReferencedDataNature;
+import net.sf.taverna.t2.reference.StreamToValueConverterSPI;
+
+/**
+ * @author alanrw
+ *
+ */
+public class StreamToBooleanConverter implements StreamToValueConverterSPI<Boolean> {
+
+	public Class<Boolean> getPojoClass() {
+		return Boolean.class;
+	}
+
+	public Boolean renderFrom(InputStream stream,
+			ReferencedDataNature dataNature, String charset) {
+		StreamToStringConverter stringConverter = new StreamToStringConverter();
+		String s = stringConverter.renderFrom(stream, dataNature, charset);
+		Boolean result  = Boolean.valueOf(s);
+		return result;
+		
+	}
+
+}
