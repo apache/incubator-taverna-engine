73196555f6d2c564f0946cf8edb5526dcc8b7f8c
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-07-25 12:10:08 +0000

    Added missing method declaration
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@13679 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/processor/iteration/IterationStrategy.java
+++ workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/processor/iteration/IterationStrategy.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (C) 2007 The University of Manchester   
- * 
+ * Copyright (C) 2007 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -32,7 +32,7 @@ public interface IterationStrategy {
 	 * the iteration strategy and the integer is the depth of the input data
 	 * collection (i.e. item depth + index array length for that item which
 	 * should be a constant).
-	 * 
+	 *
 	 * @param inputDepths
 	 *            map of port names to input collection depth
 	 * @return the length of the index array which will be generated for each
@@ -48,5 +48,7 @@ public interface IterationStrategy {
 	public Map<String, Integer> getDesiredCardinalities();
 
 	public TerminalNode getTerminalNode();
+
+    public void normalize();
 
 }
