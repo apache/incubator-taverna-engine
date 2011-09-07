53b517be17e7da97b69f963d501b6116591b9bd2
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-09-07 12:25:08 +0100

    Save provenance

---
+++ prov-taverna-activity-ui/src/main/java/no/s11/w3/prov/taverna/ui/SaveProvAction.java
@@ -0,0 +1,200 @@
+package no.s11.w3.prov.taverna.ui;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+
+import net.sf.taverna.t2.lang.results.ResultsUtils;
+import net.sf.taverna.t2.provenance.api.ProvenanceAccess;
+import net.sf.taverna.t2.provenance.client.ProvenanceExporter;
+import net.sf.taverna.t2.reference.ErrorDocument;
+import net.sf.taverna.t2.reference.ExternalReferenceSPI;
+import net.sf.taverna.t2.reference.Identified;
+import net.sf.taverna.t2.reference.IdentifiedList;
+import net.sf.taverna.t2.reference.ReferenceSet;
+import net.sf.taverna.t2.reference.ReferencedDataNature;
+import net.sf.taverna.t2.reference.T2Reference;
+import net.sf.taverna.t2.workbench.icons.WorkbenchIcons;
+import net.sf.taverna.t2.workbench.reference.config.DataManagementConfiguration;
+import net.sf.taverna.t2.workbench.views.results.saveactions.SaveAllResultsSPI;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import eu.medsea.mimeutil.MimeType;
+
+public class SaveProvAction extends SaveAllResultsSPI {
+
+	public SaveProvAction(){
+		super();
+		putValue(NAME, "Save provenance");
+		putValue(SMALL_ICON, WorkbenchIcons.saveAllIcon);
+	}
+	
+	public AbstractAction getAction() {
+		return this;
+	}
+	
+	@Override
+	public void setProvenanceEnabledForRun(boolean isProvenanceEnabledForRun) {
+		super.setProvenanceEnabledForRun(isProvenanceEnabledForRun);
+		setEnabled(isProvenanceEnabledForRun);
+	}	
+	
+	/**
+	 * Saves the result data as a file structure 
+	 * @throws IOException 
+	 */
+	protected void saveData(File file) throws IOException {
+		// First convert map of references to objects into a map of real result objects
+//		for (String portName : chosenReferences.keySet()) {
+//			writeToFileSystem(chosenReferences.get(portName), file, portName);
+//		}
+//		
+		String connectorType = DataManagementConfiguration.getInstance()
+				.getConnectorType();
+		ProvenanceAccess provenanceAccess = new ProvenanceAccess(connectorType, getContext());
+		W3ProvenanceExport export = new W3ProvenanceExport(provenanceAccess);
+		BufferedOutputStream outStream = new BufferedOutputStream(
+				new FileOutputStream(file));		
+		try {
+			export.exportAsW3Prov(getRunId(), outStream);
+		} catch (Exception e) {
+			logger.error("Failed to save the provenance graph to " + file, e);
+			JOptionPane.showMessageDialog(null,
+					"Failed to save the provenance graph to " + file,
+					"Failed to save provenance graph", JOptionPane.ERROR_MESSAGE);
+		} finally {
+			try {
+				outStream.close();
+			} catch (IOException e) {
+			}
+		}
+		
+	}
+	
+	public File writeToFileSystem(T2Reference ref, File destination, String name)
+			throws IOException {
+		Identified identified = referenceService.resolveIdentifier(ref, null,
+				context);
+
+		String fileExtension = "";
+		if (identified instanceof ReferenceSet) {
+
+		} else if (identified instanceof ErrorDocument) {
+			fileExtension = ".err";
+		}
+		
+		File writtenFile = writeObjectToFileSystem(destination, name,
+				ref, fileExtension);
+		return writtenFile;
+	}
+
+	/**
+	 * Write a specific object to the filesystem this has no access to metadata
+	 * about the object and so is not particularly clever. A File object
+	 * representing the file or directory that has been written is returned.
+	 */
+	public File writeObjectToFileSystem(File destination, String name,
+			T2Reference ref, String defaultExtension) throws IOException {
+		// If the destination is not a directory then set the destination
+		// directory to the parent and the name to the filename
+		// i.e. if the destination is /tmp/foo.text and this exists
+		// then set destination to /tmp/ and name to 'foo.text'
+		if (destination.exists() && destination.isFile()) {
+			name = destination.getName();
+			destination = destination.getParentFile();
+		}
+		if (destination.exists() == false) {
+			// Create the directory structure if not already present
+			destination.mkdirs();
+		}
+		File writtenFile = writeDataObject(destination, name, ref,
+				defaultExtension);
+		return writtenFile;
+	}
+
+	private File writeDataObject(File destination, String name,
+			T2Reference ref, String defaultExtension) throws IOException {
+		Identified identified = referenceService.resolveIdentifier(ref, null,
+				context);
+
+		if (identified instanceof IdentifiedList) {
+			// Create a new directory, iterate over the collection recursively
+			// calling this method
+			File targetDir = new File(destination.toString() + File.separatorChar + name);
+			targetDir.mkdir();
+			int count = 0;
+			List<T2Reference> elements = referenceService.getListService().getList(ref);
+			for (T2Reference subRef : elements) {
+				writeDataObject(targetDir, "" + count++, subRef,
+						defaultExtension);
+			}
+			return targetDir;
+		}
+
+		else {
+			String fileExtension = ".text";
+			if (identified instanceof ReferenceSet) {
+				List<MimeType> mimeTypes = new ArrayList<MimeType>();
+				ReferenceSet referenceSet = (ReferenceSet) identified;
+				List<ExternalReferenceSPI> externalReferences = new ArrayList<ExternalReferenceSPI>(
+						referenceSet.getExternalReferences());
+				Collections.sort(externalReferences,
+						new Comparator<ExternalReferenceSPI>() {
+							public int compare(ExternalReferenceSPI o1,
+									ExternalReferenceSPI o2) {
+								return (int) (o1.getResolutionCost() - o2
+										.getResolutionCost());
+							}
+						});
+				for (ExternalReferenceSPI externalReference : externalReferences) {
+					if (externalReference.getDataNature().equals(ReferencedDataNature.TEXT)) {
+						break;
+					}
+					mimeTypes.addAll(ResultsUtils.getMimeTypes(
+							externalReference, context));
+				}
+				if (!mimeTypes.isEmpty()) {
+
+					// Check for the most interesting type, if defined
+					String interestingType = mimeTypes.get(0).toString();
+
+					if (interestingType != null
+							&& interestingType.equals("text/plain") == false) {
+						// MIME types look like 'foo/bar'
+						String lastPart = interestingType.split("/")[1];
+						if (lastPart.startsWith("x-") == false) {
+							fileExtension = "." + lastPart;
+						}
+					}
+				}
+				File targetFile = new File(destination.toString()
+						+ File.separatorChar + name + fileExtension);
+				IOUtils.copyLarge(externalReferences.get(0)
+						.openStream(context), new FileOutputStream(targetFile));
+				return targetFile;
+			} else {
+				File targetFile = new File(destination.toString()
+						+ File.separatorChar + name + ".err");
+				FileUtils.writeStringToFile(targetFile, ((ErrorDocument) identified).getMessage());
+				return targetFile;
+			}
+
+		}
+	}
+
+	@Override
+	protected String getFilter() {
+		return "ttl";
+	}
+	
+}
---
+++ prov-taverna-activity-ui/src/main/java/no/s11/w3/prov/taverna/ui/W3ProvenanceExport.java
@@ -0,0 +1,220 @@
+package no.s11.w3.prov.taverna.ui;
+
+import java.io.BufferedOutputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.xml.namespace.QName;
+
+import net.sf.taverna.raven.appconfig.ApplicationConfig;
+import net.sf.taverna.raven.appconfig.ApplicationRuntime;
+import net.sf.taverna.raven.spi.Profile;
+import net.sf.taverna.t2.provenance.api.ProvenanceAccess;
+import net.sf.taverna.t2.provenance.lineageservice.URIGenerator;
+import net.sf.taverna.t2.provenance.lineageservice.utils.DataflowInvocation;
+import net.sf.taverna.t2.provenance.lineageservice.utils.Port;
+import net.sf.taverna.t2.provenance.lineageservice.utils.ProcessorEnactment;
+import net.sf.taverna.t2.provenance.lineageservice.utils.ProvenanceProcessor;
+import net.sf.taverna.t2.reference.T2Reference;
+import no.s11.w3.prov.elmo.Agent;
+import no.s11.w3.prov.elmo.Entity;
+import no.s11.w3.prov.elmo.ProcessExecution;
+import no.s11.w3.prov.elmo.ProvenanceContainer;
+
+import org.openrdf.elmo.ElmoModule;
+import org.openrdf.elmo.sesame.SesameManager;
+import org.openrdf.elmo.sesame.SesameManagerFactory;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.contextaware.ContextAwareConnection;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.helpers.OrganizedRDFWriter;
+import org.openrdf.rio.n3.N3Writer;
+import org.openrdf.rio.rdfxml.util.OrganizedRDFXMLWriter;
+
+public class W3ProvenanceExport {
+
+	private ProvenanceAccess provenanceAccess;
+
+	public W3ProvenanceExport() {
+	}
+
+	private static ProvenanceURIGenerator uriGenerator = new ProvenanceURIGenerator();
+
+	public SesameManager makeElmoManager() {
+		ElmoModule module = new ElmoModule();
+		SesameManagerFactory factory = new SesameManagerFactory(module);
+		factory.setInferencingEnabled(true);
+		return factory.createElmoManager();
+	}
+
+	public W3ProvenanceExport(ProvenanceAccess provenanceAccess) {
+		this.setProvenanceAccess(provenanceAccess);
+	}
+
+	private static final class ProvenanceURIGenerator extends URIGenerator {
+
+
+		public String makeDataflowInvocationURI(String workflowRunId,
+				String dataflowInvocationId) {
+			return makeWFInstanceURI(workflowRunId) + "workflow/" + dataflowInvocationId + "/";
+		}
+
+		public String makeProcessExecution(String workflowRunId,
+				String processEnactmentId) {
+			return makeWFInstanceURI(workflowRunId) + "process/" + processEnactmentId + "/";
+		}
+	}
+
+	enum Direction {
+		INPUTS, OUTPUTS;
+	}
+
+	public void exportAsW3Prov(String workflowRunId, BufferedOutputStream outStream)
+			throws RepositoryException, RDFHandlerException {
+
+		SesameManager elmoManager = makeElmoManager();
+		String runURI = uriGenerator.makeWFInstanceURI(workflowRunId);
+		// FIXME: Should this be "" to indicate the current file?
+		ProvenanceContainer provContainer = elmoManager.create(
+				new QName(runURI, "provenanceContainer"), ProvenanceContainer.class);
+		// TODO: Link provContainer to anything?
+		//elmoManager.persist(provContainer);
+		
+		// Mini-provenance about this provenance trace
+		String versionName = ApplicationConfig.getInstance().getName();
+		Agent tavernaAgent = elmoManager.create(
+				new QName("http://ns.taverna.org.uk/2011/software/", versionName), Agent.class);
+		ProcessExecution storeProvenance = elmoManager.create(ProcessExecution.class);
+		storeProvenance.getProvenanceOntologyIsControlledBy().add(tavernaAgent);
+		tavernaAgent.getProvenanceOntologyIsParticipantIn().add(storeProvenance);
+		provContainer.getProvenanceOntologyIsGeneratedBy().add(storeProvenance);
+		// The store-provenance-process used the workflow run as input
+		storeProvenance.getProvenanceOntologyUsed().add(elmoManager.create(new QName(runURI), Entity.class, ProcessExecution.class));
+	//	elmoManager.persist(provContainer);
+		//elmoManager.persist(storeProvenance);
+	
+		
+		DataflowInvocation dataflowInvocation = provenanceAccess.getDataflowInvocation(workflowRunId);
+		//String dataflowURI = uriGenerator.makeDataflowInvocationURI(workflowRunId, dataflowInvocation.getDataflowInvocationId());
+		ProcessExecution wfProcess = elmoManager.create(new QName(runURI), ProcessExecution.class, Agent.class);
+		wfProcess.getProvenanceOntologyIsControlledBy().add(tavernaAgent);
+		// inverse property
+		tavernaAgent.getProvenanceOntologyIsParticipantIn().add(wfProcess);
+		// TODO: start, stop?
+
+		// Workflow inputs and outputs
+		storeEntitities(dataflowInvocation.getInputsDataBindingId(), wfProcess,
+				Direction.INPUTS, elmoManager);
+		// FIXME: These entities come out as "generated" by multiple processes
+		storeEntitities(dataflowInvocation.getOutputsDataBindingId(), wfProcess,
+				Direction.OUTPUTS, elmoManager);
+//		elmoManager.persist(wfProcess);
+		
+		
+		List<ProcessorEnactment> processorEnactments = provenanceAccess
+				.getProcessorEnactments(workflowRunId);
+		// This will also include processor enactments in nested workflows
+		for (ProcessorEnactment pe : processorEnactments) {
+			String parentURI = pe.getParentProcessorEnactmentId();
+			if (parentURI == null) {
+				// Top-level workflow
+				parentURI = runURI;
+			} else {
+				// inside nested wf - this will be parent processenactment
+				parentURI = uriGenerator.makeProcessExecution(
+						pe.getWorkflowRunId(), pe.getProcessEnactmentId());
+			}
+			String processURI = uriGenerator.makeProcessExecution(
+					pe.getWorkflowRunId(), pe.getProcessEnactmentId());
+			ProcessExecution process = elmoManager.create(
+					new QName(processURI), ProcessExecution.class);
+			Agent parentProcess = elmoManager.designate(new QName(parentURI), Agent.class, ProcessExecution.class);
+			process.getProvenanceOntologyIsControlledBy().add(parentProcess);
+			// inverse property
+			parentProcess.getProvenanceOntologyIsParticipantIn().add(process);
+			
+			// TODO: start, stop?
+
+			// TODO: work out preceeding and controlling from workflow definitions
+
+			// TODO: Linking this to the processor in the workflow definition?
+
+			// TODO: How to link together iterations on a single processor and the collections
+			// they are iterating over and creating? 
+			// Need 'virtual' ProcessExecution for iteration.
+			
+			// TODO: Activity/service details from definition?
+			
+			// Inputs and outputs
+			storeEntitities(pe.getInitialInputsDataBindingId(), process,
+					Direction.INPUTS, elmoManager);
+			storeEntitities(pe.getFinalOutputsDataBindingId(), process,
+					Direction.OUTPUTS, elmoManager);
+
+//			elmoManager.persist(process);
+		}
+
+		// Save the whole thing
+		ContextAwareConnection connection = elmoManager.getConnection();
+		connection.setNamespace("scufl2",
+				"http://ns.taverna.org.uk/2010/scufl2#");
+		connection
+				.setNamespace("prov", "http://w3.org/ProvenanceOntology.owl#");
+		connection.setNamespace("owl", "http://www.w3.org/2002/07/owl#");
+		connection.export(new OrganizedRDFWriter(new N3Writer(outStream)));
+
+	}
+
+	private String getProcessorName(String processorId) {		
+		ProvenanceProcessor processor = provenanceAccess.getProvenanceProcessor(processorId);
+		// TODO: Cache same processorId?
+		return processor.getProcessorName();
+	}
+
+	private void storeEntitities(String dataBindingId,
+			ProcessExecution process, Direction direction,
+			SesameManager elmoManager) {
+
+		Map<Port, T2Reference> inputs = provenanceAccess
+				.getDataBindings(dataBindingId);
+		for (Entry<Port, T2Reference> inputEntry : inputs.entrySet()) {
+			Port port = inputEntry.getKey();
+			T2Reference t2Ref = inputEntry.getValue();
+
+			String dataURI = uriGenerator.makeT2ReferenceURI(t2Ref.toUri()
+					.toASCIIString());
+
+			Entity entity = elmoManager
+					.create(new QName(dataURI), Entity.class);
+
+			if (direction == Direction.INPUTS) {
+				process.getProvenanceOntologyUsed().add(entity);
+				// This is the inverse super-property, we should not need to do
+				// this
+				// if inferencing is turned on
+				entity.getProvenanceOntologyIsParticipantIn().add(process);
+			} else {
+				entity.getProvenanceOntologyIsGeneratedBy().add(process);
+				// No equivalent inverse property in process!
+			}
+			String portURI = uriGenerator.makePortURI(port.getWorkflowId(),
+					port.getProcessorName(), port.getPortName(),
+					port.isInputPort());
+			// when was it generated/used?
+			// in which role? (ie. port)
+
+//			elmoManager.persist(entity);
+		}
+
+	}
+
+	public ProvenanceAccess getProvenanceAccess() {
+		return provenanceAccess;
+	}
+
+	public void setProvenanceAccess(ProvenanceAccess provenanceAccess) {
+		this.provenanceAccess = provenanceAccess;
+	}
+
+}
