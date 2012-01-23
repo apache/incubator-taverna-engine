0c2184b98f4ac3c3d7be8ef67654634fff3fe5e6
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-01-23 13:53:17 +0000

    Added DataService
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/platform/trunk/platform@14427 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ data/src/main/java/uk/org/taverna/platform/data/impl/DataImpl.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (C) 2011 The University of Manchester
+ *
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package uk.org.taverna.platform.data.impl;
+
+import java.net.URI;
+import java.util.List;
+
+import uk.org.taverna.platform.data.Data;
+
+/**
+ *
+ *
+ * @author David Withers
+ */
+public class DataImpl implements Data {
+
+	private String ID;
+
+	private URI reference;
+
+	private Object value;
+
+	private Data container;
+
+	private List<Data> elements;
+
+	private String mimeType;
+
+	public DataImpl(String ID, Object value) {
+		this.value = value;
+	}
+
+	public DataImpl(String ID, URI reference) {
+		this.reference = reference;
+	}
+
+	public DataImpl(String ID, URI reference, Object value) {
+		this.reference = reference;
+		this.value = value;
+	}
+
+	public DataImpl(String ID, List<Data> elements) {
+		this.elements = elements;
+//		for (Data element : elements) {
+//			element.container = this;
+//		}
+	}
+
+	@Override
+	public String getID() {
+		return ID;
+	}
+
+	@Override
+	public int getDepth() {
+		int depth = 0;
+		if (elements != null) {
+			depth++;
+			if (!elements.isEmpty()) {
+				depth += elements.get(0).getDepth();
+			}
+		}
+		return depth;
+	}
+
+	@Override
+	public boolean isReference() {
+		return reference != null;
+	}
+
+	@Override
+	public URI getReference() {
+		return reference;
+	}
+
+	@Override
+	public Object getValue() {
+		return value;
+	}
+
+	@Override
+	public Data getContainer() {
+		return container;
+	}
+
+	@Override
+	public List<Data> getElements() {
+		return elements;
+	}
+
+}
---
+++ data/src/main/java/uk/org/taverna/platform/data/impl/DataServiceImpl.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (C) 2011 The University of Manchester
+ *
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package uk.org.taverna.platform.data.impl;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import uk.org.taverna.platform.data.Data;
+import uk.org.taverna.platform.data.DataService;
+
+/**
+ *
+ *
+ * @author David Withers
+ */
+public class DataServiceImpl implements DataService {
+
+	private Map<String, Data> store = new HashMap<String, Data>();
+
+	@Override
+	public Data get(String ID) {
+		return store.get(ID);
+	}
+
+	@Override
+	public boolean delete(String ID) {
+		return store.remove(ID) != null;
+	}
+
+	@Override
+	public Data create(Object value) {
+		if (value instanceof List) {
+			List<?> listValue = (List<?>) value;
+			List<Data> dataList = new ArrayList<Data>();
+			for (Object object : listValue) {
+				dataList.add(create(object));
+			}
+			return store(new DataImpl(UUID.randomUUID().toString(), dataList));
+		} else if (value instanceof URI) {
+			return store(new DataImpl(UUID.randomUUID().toString(), (URI) value));
+		} else {
+			return store(new DataImpl(UUID.randomUUID().toString(), value));
+		}
+	}
+
+	private Data store(Data data) {
+		store.put(data.getID(), data);
+		return data;
+	}
+
+}
