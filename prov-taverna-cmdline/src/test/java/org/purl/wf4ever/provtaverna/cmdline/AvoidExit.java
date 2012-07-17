9e65a10986a96a3ae81b92c592516f096c631b90
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-07-17 14:57:23 +0100

    Javadocs for AvoidExit

--- prov-taverna-cmdline/src/test/java/org/purl/wf4ever/provtaverna/cmdline/AvoidExit.java
+++ prov-taverna-cmdline/src/test/java/org/purl/wf4ever/provtaverna/cmdline/AvoidExit.java
@@ -3,10 +3,32 @@ package org.purl.wf4ever.provtaverna.cmdline;
 import java.security.Permission;
 import java.util.Stack;
 
+/**
+ * A security manager that disallows System.exit().
+ * <p>
+ * Methods calling {@link System#exit(int)} will instead fail with a
+ * {@link ExitNotAllowed} exception.
+ * <p>
+ * To install this security manager, use {@link AvoidExit#install()} - and then
+ * call {@link AvoidExit#uninstall()} on the returned instance, which will
+ * restore the original security manager.
+ * <p>
+ * The installation and removal of security managers is by its nature NOT thread
+ * safe. It is therefore not advisable to use {@link AvoidExit#install()} or
+ * {@link AvoidExit#uninstall()} from more than one thread - as otherwise the
+ * wrong 'old' security manager could be set.
+ * <p>
+ * In case this happens, this class will always revert to allowing System.exit
+ * after {@link #uninstall()} has been called, in order for
+ * {@link RuntimeException}s to cause normal exit of the JVM.
+ * 
+ * @author Stian Soiland-Reyes
+ * 
+ */
 public final class AvoidExit extends SecurityManager {
 
 	boolean exitAllowed = false;
-	
+
 	public static class ExitNotAllowed extends SecurityException {
 		public ExitNotAllowed() {
 			super("Exit not allowed");
@@ -22,14 +44,15 @@ public final class AvoidExit extends SecurityManager {
 
 	@Override
 	public void checkExit(int status) {
-		if (! exitAllowed) {
+		if (!exitAllowed) {
 			throw new ExitNotAllowed();
 		}
 	}
 
 	@Override
-	public void checkPermission(Permission perm) {		
-		if (perm instanceof RuntimePermission && perm.getName().equals("setSecurityManager")) {			
+	public void checkPermission(Permission perm) {
+		if (perm instanceof RuntimePermission
+				&& perm.getName().equals("setSecurityManager")) {
 			// Needed to uninstall ourselves - and also it was obviously allowed
 			// before as we we got installed
 			return;
@@ -37,17 +60,37 @@ public final class AvoidExit extends SecurityManager {
 		super.checkPermission(perm);
 	}
 
+	/**
+	 * Uninstall this security manager
+	 * <p>
+	 * If the current security manager is this instance, then the security
+	 * manager that was present before this instance was installed will be
+	 * reinstated as the current security manager.
+	 * <p>
+	 * If the current security manager is another instance, then an error
+	 * message is printed on System.err and the current security manager
+	 * is not modified.
+	 * <p>
+	 * After calling uninstall, {@link System#exit(int)} will always be
+	 * allowed by this instance.
+	 * 
+	 */
 	public void uninstall() {
 		// Fallback in case we are not removed properly, then allow exit again
 		exitAllowed = true;
 		if (System.getSecurityManager() != this) {
 			// Note: AVOID exceptions here - as they might force an exit
-			System.err.println("Can't uninstall AvoidExit; not current security manager");
+			System.err
+					.println("Can't uninstall AvoidExit; no longer current security manager");
 			return;
 		}
-		System.setSecurityManager(oldSecurityManager);		
+		System.setSecurityManager(oldSecurityManager);
 	}
 
+	/**
+	 * Install an AvoidExit as a security manager
+	 * 
+	 */
 	public static AvoidExit install() {
 		return new AvoidExit();
 	}
