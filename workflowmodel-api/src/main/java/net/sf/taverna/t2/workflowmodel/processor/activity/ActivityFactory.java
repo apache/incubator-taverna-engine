3603cfcb5b13334c4ce08ffae70132da5004be64
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-06-28 09:46:02 +0000

    Added method to get JSON Schema from Activity and Dispatch Layer factories.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@15811 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/processor/activity/ActivityFactory.java
+++ workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/processor/activity/ActivityFactory.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (C) 2010 The University of Manchester   
- * 
+ * Copyright (C) 2010 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -22,32 +22,34 @@ package net.sf.taverna.t2.workflowmodel.processor.activity;
 
 import java.net.URI;
 
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+
 /**
  * Factory for creating {@link Activity} instances.
- * 
+ *
  * @author David Withers
  */
 public interface ActivityFactory {
 
 	/**
-	 * Creates a new <code>Activity</code>.
-	 * 
-	 * @return a new <code>Activity</code>
+	 * Creates a new <code>Activity</code> instance.
+	 *
+	 * @return a new <code>Activity</code> instance
 	 */
 	public Activity<?> createActivity();
 
 	/**
-	 * Creates a new activity configuration.
-	 * 
-	 * @return a new activity configuration
+	 * Returns the type of the <code>Activity</code>s that this factory can create.
+	 *
+	 * @return the type of the <code>Activity</code>s that this factory can create
 	 */
-	public Object createActivityConfiguration();
+	public URI getActivityType();
 
 	/**
-	 * Returns the URI that identifies the <code>Activity</code> type.
-	 * 
-	 * @return the URI that identifies the <code>Activity</code> type
+	 * Returns the JSON Schema for the configuration required by the <code>Activity</code>.
+	 *
+	 * @return the JSON Schema for the configuration required by the <code>Activity</code>
 	 */
-	public URI getActivityURI();
+	public JsonSchema getActivityConfigurationSchema();
 
 }
--- workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/processor/dispatch/DispatchLayerFactory.java
+++ workflowmodel-api/src/main/java/net/sf/taverna/t2/workflowmodel/processor/dispatch/DispatchLayerFactory.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (C) 2011 The University of Manchester   
- * 
+ * Copyright (C) 2011 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -23,17 +23,38 @@ package net.sf.taverna.t2.workflowmodel.processor.dispatch;
 import java.net.URI;
 import java.util.Set;
 
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+
 /**
  * Factory for creating {@link DispatchLayer} instances.
- * 
+ *
  * @author David Withers
  */
 public interface DispatchLayerFactory {
 
-	public DispatchLayer<?> createDispatchLayer(URI uri);
-	
-	public Object createDispatchLayerConfiguration(URI uri);
-	
-	public Set<URI> getDispatchLayerURIs();
+	/**
+	 * Creates a new <code>DispatchLayer</code> instance.
+	 *
+	 * @param dispatchLayerType
+	 *            the type of the <code>DispatchLayer</code>
+	 * @return a new <code>DispatchLayer</code> instance
+	 */
+	public DispatchLayer<?> createDispatchLayer(URI dispatchLayerType);
+
+	/**
+	 * Returns the types of the <code>DispatchLayer</code>s that this factory can create.
+	 *
+	 * @return the types of the <code>DispatchLayer</code>s that this factory can create
+	 */
+	public Set<URI> getDispatchLayerTypes();
+
+	/**
+	 * Returns the JSON Schema for the configuration required by the <code>DispatchLayer</code>.
+	 *
+	 * @param dispatchLayerType
+	 *            the type of the <code>DispatchLayer</code>
+	 * @return the JSON Schema for the configuration required by the <code>DispatchLayer</code>
+	 */
+	public JsonSchema getDispatchLayerConfigurationSchema(URI dispatchLayerType);
 
 }
