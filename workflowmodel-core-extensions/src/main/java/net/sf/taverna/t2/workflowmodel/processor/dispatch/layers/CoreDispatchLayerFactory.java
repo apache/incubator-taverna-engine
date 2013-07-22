83eba1b6dd8c05716ae97e171bf70d59683e70dd
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-07-22 09:20:47 +0000

    Updated for API changes.
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.core/trunk@15867 bf327186-88b3-11dd-a302-d386e5130c1c

--- workflowmodel-core-extensions/src/main/java/net/sf/taverna/t2/workflowmodel/processor/dispatch/layers/CoreDispatchLayerFactory.java
+++ workflowmodel-core-extensions/src/main/java/net/sf/taverna/t2/workflowmodel/processor/dispatch/layers/CoreDispatchLayerFactory.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
- * Copyright (C) 2011 The University of Manchester   
- * 
+ * Copyright (C) 2011 The University of Manchester
+ *
  *  Modifications to the initial code base are copyright of their
  *  respective authors, or their employers as appropriate.
- * 
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1 of
  *  the License, or (at your option) any later version.
- *    
+ *
  *  This program is distributed in the hope that it will be useful, but
  *  WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- *    
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -27,9 +27,11 @@ import java.util.Set;
 import net.sf.taverna.t2.workflowmodel.processor.dispatch.DispatchLayer;
 import net.sf.taverna.t2.workflowmodel.processor.dispatch.DispatchLayerFactory;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 /**
  * Factory for creating core dispatch layers.
- * 
+ *
  * The core dispatch layers are :
  * <ul>
  * <li>ErrorBounce</li>
@@ -41,7 +43,7 @@ import net.sf.taverna.t2.workflowmodel.processor.dispatch.DispatchLayerFactory;
  * <li>Loop</li>
  * <li>IntermediateProvenance</li>
  * </ul>
- * 
+ *
  * @author David Withers
  */
 public class CoreDispatchLayerFactory implements DispatchLayerFactory {
@@ -92,20 +94,13 @@ public class CoreDispatchLayerFactory implements DispatchLayerFactory {
 	}
 
 	@Override
-	public Object createDispatchLayerConfiguration(URI uri) {
-		Object dispatchLayerConfiguration = null;
-		if (parallelizeLayer.equals(uri)) {
-			dispatchLayerConfiguration = new ParallelizeConfig();
-		} else if (retryLayer.equals(uri)) {
-			dispatchLayerConfiguration = new RetryConfig();
-		} else if (loopLayer.equals(uri)) {
-			dispatchLayerConfiguration = new LoopConfiguration();
-		}
-		return dispatchLayerConfiguration;
+	public JsonNode getDispatchLayerConfigurationSchema(URI uri) {
+		// TODO
+		return null;
 	}
 
 	@Override
-	public Set<URI> getDispatchLayerURIs() {
+	public Set<URI> getDispatchLayerTypes() {
 		return dispatchLayerURIs;
 	}
 
