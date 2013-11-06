0571bf851e793f68939a3b58ed38174378555ff0
stian@mygrid.org.uk <stian@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2013-11-06 17:47:28 +0000

    T3-935 JSON serialization options to avoid empty arrays
    Save using DataBundles getWorkflowRunReport().
    
    git-svn-id: https://taverna.googlecode.com/svn/unsorted/platform/trunk/platform@16278 bf327186-88b3-11dd-a302-d386e5130c1c

--- taverna-report-api/src/main/java/uk/org/taverna/platform/report/WorkflowReportJSON.java
+++ taverna-report-api/src/main/java/uk/org/taverna/platform/report/WorkflowReportJSON.java
@@ -9,6 +9,8 @@ import java.nio.file.StandardOpenOption;
 
 import org.purl.wf4ever.robundle.manifest.Manifest.PathMixin;
 
+import uk.org.taverna.databundle.DataBundles;
+
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -18,22 +20,27 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public class WorkflowReportJSON {
     
     public void save(WorkflowReport wfReport, Path path) throws IOException {
-        ObjectNode objNode = save(wfReport);
+//        ObjectNode objNode = save(wfReport);
         
-        injectContext(objNode);
+//        injectContext(objNode);
         
         ObjectMapper om = makeObjectMapper();
+//        Files.createFile(path);
         try (Writer w = Files.newBufferedWriter(path,
                 Charset.forName("UTF-8"), StandardOpenOption.WRITE,
-                StandardOpenOption.TRUNCATE_EXISTING)) {
+                StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
             om.writeValue(w, wfReport);
         }
     }
 
-    private ObjectMapper makeObjectMapper() {
+    protected static ObjectMapper makeObjectMapper() {
         ObjectMapper om = new ObjectMapper();
         om.enable(SerializationFeature.INDENT_OUTPUT);
         om.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        om.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+        om.disable(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS);
+        om.disable(SerializationFeature.WRITE_NULL_MAP_VALUES);
+        om.disable(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED);        
         om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         om.disable(SerializationFeature.WRITE_NULL_MAP_VALUES);
         om.addMixInAnnotations(Path.class, PathMixin.class);
@@ -47,8 +54,9 @@ public class WorkflowReportJSON {
         objNode.with("@context").put("prov", "http://www.w3.org/ns/prov#");
     }
 
-    private ObjectNode save(WorkflowReport wfReport) {
-        return JsonNodeFactory.instance.objectNode();        
+    public void save(WorkflowReport wfReport) throws IOException {
+        Path path = DataBundles.getWorkflowRunReport(wfReport.getDataBundle());
+        save(wfReport, path);
     }
 
 }
