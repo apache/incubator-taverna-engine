e388ce29bae532569d5c7aca1316421cba02c9ea
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2014-01-07 16:53:54 +0000

    Document filter for names
    git-svn-id: https://taverna.googlecode.com/svn/taverna/utils/net.sf.taverna.t2.lang/branches/maintenance@16489 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ ui/src/main/java/net/sf/taverna/t2/lang/ui/SanitisingDocumentFilter.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.lang.ui;
+
+import java.util.regex.Pattern;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+
+/**
+ * @author alanrw
+ *
+ */
+public class SanitisingDocumentFilter extends DocumentFilter {
+	public void insertString(DocumentFilter.FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+		
+		fb.insertString(offset, sanitiseString(string), attr);
+	}
+	
+	public void replace(DocumentFilter.FilterBypass fb, int offset, int length,
+		      String text, javax.swing.text.AttributeSet attr)
+
+		      throws BadLocationException {
+		           fb.insertString(offset, sanitiseString(text), attr);   
+		 }
+	
+	private static String sanitiseString(String text) {
+		String result = text;
+		if (Pattern.matches("\\w++", text) == false) {
+			result = "";
+			for (char c : text.toCharArray()) {
+				if (Character.isLetterOrDigit(c) || c == '_') {
+					result += c;
+				} else {
+					result += "_";
+				}
+			}
+		}
+		return result;		
+	}
+}
