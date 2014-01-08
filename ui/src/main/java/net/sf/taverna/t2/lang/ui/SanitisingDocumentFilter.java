7f23d78af0cdc6fc8d0f4527de4a342b6d998635
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2014-01-08 14:36:34 +0000

    Changed filter so it can be easily applied to components
    git-svn-id: https://taverna.googlecode.com/svn/taverna/utils/net.sf.taverna.t2.lang/branches/maintenance@16494 bf327186-88b3-11dd-a302-d386e5130c1c

--- ui/src/main/java/net/sf/taverna/t2/lang/ui/SanitisingDocumentFilter.java
+++ ui/src/main/java/net/sf/taverna/t2/lang/ui/SanitisingDocumentFilter.java
@@ -5,15 +5,32 @@ package net.sf.taverna.t2.lang.ui;
 
 import java.util.regex.Pattern;
 
+import javax.swing.text.AbstractDocument;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 import javax.swing.text.DocumentFilter;
+import javax.swing.text.JTextComponent;
 
 /**
  * @author alanrw
  *
  */
 public class SanitisingDocumentFilter extends DocumentFilter {
+	
+	private static SanitisingDocumentFilter INSTANCE = new SanitisingDocumentFilter();
+	
+	private SanitisingDocumentFilter () {
+		super();
+	}
+	
+	public static void addFilterToComponent(JTextComponent c) {
+		Document d = c.getDocument();
+		if (d instanceof AbstractDocument) {
+			((AbstractDocument) d).setDocumentFilter(INSTANCE);
+		} 		
+	}
+	
 	public void insertString(DocumentFilter.FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
 		
 		fb.insertString(offset, sanitiseString(string), attr);
@@ -23,7 +40,7 @@ public class SanitisingDocumentFilter extends DocumentFilter {
 		      String text, javax.swing.text.AttributeSet attr)
 
 		      throws BadLocationException {
-		           fb.insertString(offset, sanitiseString(text), attr);   
+		           fb.replace(offset, length, sanitiseString(text), attr);   
 		 }
 	
 	private static String sanitiseString(String text) {
