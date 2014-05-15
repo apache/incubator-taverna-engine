e6ffef063e525ac444596352fe080ae74c21981b
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-05-15 15:50:39 +0100

    Organized imports

--- workflowmodel-impl/src/main/java/net/sf/taverna/t2/invocation/impl/InvocationContextImpl.java
+++ workflowmodel-impl/src/main/java/net/sf/taverna/t2/invocation/impl/InvocationContextImpl.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import java.util.List;
 
 import net.sf.taverna.t2.invocation.InvocationContext;
-import net.sf.taverna.t2.invocation.InvocationContextEntitySource;
 import net.sf.taverna.t2.provenance.reporter.ProvenanceReporter;
 import net.sf.taverna.t2.reference.ReferenceService;
 
