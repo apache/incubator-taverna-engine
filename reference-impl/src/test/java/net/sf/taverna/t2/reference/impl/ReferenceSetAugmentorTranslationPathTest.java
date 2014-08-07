da446c59d751e683b222b87d9afc5ef17ddf2f8a
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-08-07 18:03:10 +0100

    Avoid warnings

--- reference-impl/src/test/java/net/sf/taverna/t2/reference/impl/ReferenceSetAugmentorTranslationPathTest.java
+++ reference-impl/src/test/java/net/sf/taverna/t2/reference/impl/ReferenceSetAugmentorTranslationPathTest.java
@@ -1,9 +1,9 @@
 package net.sf.taverna.t2.reference.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 
@@ -19,6 +19,7 @@ import org.junit.Test;
 
 public class ReferenceSetAugmentorTranslationPathTest {
 
+	@SuppressWarnings("rawtypes")
 	private final class DummyTranslator implements
 			ExternalReferenceTranslatorSPI {
 		@Override
