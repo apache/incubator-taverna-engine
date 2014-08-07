b6a84d48f0ee5d893a9b7e8e75ac284edd500fc9
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-08-07 18:33:04 +0100

    Use testhelpers for translation path test

--- reference-impl/src/test/java/net/sf/taverna/t2/reference/impl/ReferenceSetAugmentorTranslationPathTest.java
+++ reference-impl/src/test/java/net/sf/taverna/t2/reference/impl/ReferenceSetAugmentorTranslationPathTest.java
@@ -3,175 +3,48 @@ package net.sf.taverna.t2.reference.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.InputStream;
-import java.util.Collections;
 import java.util.Set;
 
-import net.sf.taverna.t2.reference.DereferenceException;
-import net.sf.taverna.t2.reference.ExternalReferenceBuilderSPI;
 import net.sf.taverna.t2.reference.ExternalReferenceSPI;
-import net.sf.taverna.t2.reference.ExternalReferenceTranslatorSPI;
 import net.sf.taverna.t2.reference.ReferenceContext;
 import net.sf.taverna.t2.reference.ReferenceSet;
-import net.sf.taverna.t2.reference.ReferencedDataNature;
-import net.sf.taverna.t2.reference.T2Reference;
+import net.sf.taverna.t2referencetest.DummyReferenceSet;
+import net.sf.taverna.t2referencetest.GreenBuilder;
+import net.sf.taverna.t2referencetest.GreenReference;
+import net.sf.taverna.t2referencetest.GreenToRed;
+import net.sf.taverna.t2referencetest.RedReference;
 
 import org.junit.Test;
 
 public class ReferenceSetAugmentorTranslationPathTest {
 
-	private final class DummyBuilder implements
-			ExternalReferenceBuilderSPI {
-		@Override
-		public ExternalReferenceSPI createReference(InputStream byteStream,
-				ReferenceContext context) {
-			return new TargetReference();
-		}
-
-		@Override
-		public Class getReferenceType() {
-			return TargetReference.class;
-		}
-
-		@Override
-		public boolean isEnabled(ReferenceContext context) {
-			return true;
-		}
-
-		@Override
-		public float getConstructionCost() {
-			return 0.5f;
-		}
-	}
-
-
-	@SuppressWarnings("rawtypes")
-	private final class DummyTranslator implements
-			ExternalReferenceTranslatorSPI {
-		@Override
-		public ExternalReferenceSPI createReference(
-				ExternalReferenceSPI sourceReference,
-				ReferenceContext context) {
-			return new TargetReference();
-		}
-
-		@Override
-		public Class getSourceReferenceType() {
-			return SourceReference.class;
-		}
-
-		@Override
-		public Class getTargetReferenceType() {
-			return TargetReference.class;
-		}
-
-		@Override
-		public boolean isEnabled(ReferenceContext context) {
-			return true;
-		}
-
-		@Override
-		public float getTranslationCost() {
-			return 1.5f;
-		}
-	}
-
-
-	private final class DummyReferenceSet implements ReferenceSet {
-		@Override
-		public T2Reference getId() {
-			return null;
-		}
-
-		@Override
-		public Set<ExternalReferenceSPI> getExternalReferences() {
-			return Collections.<ExternalReferenceSPI>singleton(new SourceReference());
-		}
-
-		@Override
-		public Long getApproximateSizeInBytes() {
-			return null;
-		}
-	}
-
-
-	class DummyReference implements
-			ExternalReferenceSPI {
-		@Override
-		public InputStream openStream(ReferenceContext context)
-				throws DereferenceException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public float getResolutionCost() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public ReferencedDataNature getDataNature() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public String getCharset() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public Long getApproximateSizeInBytes() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public ExternalReferenceSPI clone() {
-			return null;
-		}
-	}
-	
-	class SourceReference extends DummyReference {
-		
-	}
-
-	class TargetReference extends DummyReference {
-	}
-	
 	private final class ReferenceSetAugmentorWithPath extends
 			ReferenceSetAugmentorImpl {
 		protected TranslationPath path  = new TranslationPath();
 	}
 
-
-	
 	private ReferenceSetAugmentorWithPath augmentor = new ReferenceSetAugmentorWithPath();
-	
-	
 	
 	@Test
 	public void doTranslationWithTranslator() throws Exception {
 		ReferenceContext context = new EmptyReferenceContext();
-		ReferenceSet rs = new DummyReferenceSet();		
-		augmentor.path.translators.add(new DummyTranslator());
+		ReferenceSet rs = new DummyReferenceSet(new GreenReference("green"));		
+		augmentor.path.translators.add(new GreenToRed());
 		Set<ExternalReferenceSPI> set = augmentor.path.doTranslation(rs, context);		
 		assertEquals(1, set.size());
-		assertTrue(set.iterator().next() instanceof TargetReference);
+		assertTrue(set.iterator().next() instanceof RedReference);
 	}
 	
 	@Test
 	public void doTranslationByReadingStream() throws Exception {
 		ReferenceContext context = new EmptyReferenceContext();
-		ReferenceSet rs = new DummyReferenceSet();
-		augmentor.path.sourceReference = new SourceReference();
-		augmentor.path.initialBuilder = new DummyBuilder();
+		augmentor.path.sourceReference = new RedReference("red");
+		ReferenceSet rs = new DummyReferenceSet(augmentor.path.sourceReference);
+		augmentor.path.initialBuilder = new GreenBuilder();
 		//augmentor.path.translators.add(new DummyTranslator());
 		Set<ExternalReferenceSPI> set = augmentor.path.doTranslation(rs, context);		
 		assertEquals(1, set.size());
-		assertTrue(set.iterator().next() instanceof TargetReference);
+		assertTrue(set.iterator().next() instanceof GreenReference);
 	}
 
 	
