860035adb5a0ea4a6dadf84a7e92606ef7b4a90b
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-08-08 14:47:54 +0100

    test translation of "Ferronnière" between byte[] and String

--- src/test/java/net/sf/taverna/t2/reference/impl/external/object/ByteArrayToStringTest.java
+++ src/test/java/net/sf/taverna/t2/reference/impl/external/object/ByteArrayToStringTest.java
@@ -3,6 +3,7 @@ package net.sf.taverna.t2.reference.impl.external.object;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.nio.charset.Charset;
 import java.util.Set;
 
 import net.sf.taverna.t2.reference.ExternalReferenceSPI;
@@ -11,37 +12,59 @@ import net.sf.taverna.t2.reference.ReferenceSet;
 import net.sf.taverna.t2.reference.impl.EmptyReferenceContext;
 import net.sf.taverna.t2.reference.impl.TranslationPath;
 import net.sf.taverna.t2referencetest.DummyReferenceSet;
-import net.sf.taverna.t2referencetest.GreenBuilder;
-import net.sf.taverna.t2referencetest.GreenReference;
-import net.sf.taverna.t2referencetest.GreenToRed;
-import net.sf.taverna.t2referencetest.RedReference;
 
 import org.junit.Test;
 
 public class ByteArrayToStringTest {
 
 	protected TranslationPath path  = new TranslationPath();
+	private final Charset UTF8 = Charset.forName("UTF-8");
+//	
+	private final String string = "Ferronni\u00e8re";
 	
-	@Test
-	public void doTranslationWithTranslator() throws Exception {
-		ReferenceContext context = new EmptyReferenceContext();
-		ReferenceSet rs = new DummyReferenceSet(new GreenReference("green"));		
-		path.getTranslators().add(new GreenToRed());
-		Set<ExternalReferenceSPI> set = path.doTranslation(rs, context);		
-		assertEquals(1, set.size());
-		assertTrue(set.iterator().next() instanceof RedReference);
-	}
 	
+//	@Test
+//	public void doTranslationWithTranslator() throws Exception {
+//		ReferenceContext context = new EmptyReferenceContext();
+//		InlineStringReference inlineString = new InlineStringReference();
+//		inlineString.setContents("string");
+//		//path.setSourceReference(inlineString);		
+//		ReferenceSet rs = new DummyReferenceSet(inlineString);		
+//		path.getTranslators().add(new GreenToRed());
+//		Set<ExternalReferenceSPI> set = path.doTranslation(rs, context);		
+//		assertEquals(1, set.size());
+//		assertTrue(set.iterator().next() instanceof RedReference);
+//	}
+//	
 	@Test
-	public void doTranslationByReadingStream() throws Exception {
+	public void translateStringToByteArrayBuilder() throws Exception {
 		ReferenceContext context = new EmptyReferenceContext();
-		path.setSourceReference(new RedReference("red"));
+		InlineStringReference inlineString = new InlineStringReference();
+		inlineString.setContents(string);
+		path.setSourceReference(inlineString);
 		ReferenceSet rs = new DummyReferenceSet(path.getSourceReference());
-		path.setInitialBuilder(new GreenBuilder());
-		//augmentor.path.translators.add(new DummyTranslator());
+		path.setInitialBuilder(new InlineByteArrayReferenceBuilder());
 		Set<ExternalReferenceSPI> set = path.doTranslation(rs, context);		
 		assertEquals(1, set.size());
-		assertTrue(set.iterator().next() instanceof GreenReference);
+		InlineByteArrayReference byteRef = (InlineByteArrayReference) set.iterator().next();
+		
+		assertEquals(string, new String(byteRef.getValue(), UTF8));		
+	}
+
+	@Test
+	public void translateByteArrayToStringBuilder() throws Exception {
+		ReferenceContext context = new EmptyReferenceContext();
+		InlineByteArrayReference inlineByte = new InlineByteArrayReference();
+		inlineByte.setValue(string.getBytes(UTF8));
+		path.setSourceReference(inlineByte);
+		ReferenceSet rs = new DummyReferenceSet(path.getSourceReference());
+		path.setInitialBuilder(new InlineStringReferenceBuilder());
+		Set<ExternalReferenceSPI> set = path.doTranslation(rs, context);		
+		assertEquals(1, set.size());
+		assertTrue(set.iterator().next() instanceof InlineStringReference);
+		InlineStringReference inlineString = (InlineStringReference) set.iterator().next();
+		assertEquals(string,  inlineString.getContents());
+		
 	}
 	
 }
