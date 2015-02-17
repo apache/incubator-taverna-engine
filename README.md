3ecb12916215fa3efdcd9380f55f700d0eb67e95
Stian Soiland-Reyes <stain@apache.org>
2015-02-17 20:42:45 +0000

    temporarily empty repository
    .. as Software Grant is not yet formally on file

--- README.md
+++
@@ -1,3 +0,0 @@
-# Apache Taverna Engine
-
-[![Build Status](https://travis-ci.org/taverna-incubator/incubator-taverna-engine.svg)](https://travis-ci.org/taverna-incubator/incubator-taverna-engine)
--- taverna-provenanceconnector/src/main/java/net/sf/taverna/t2/provenance/ProvenanceConnectorFactory.java
+++
@@ -1,8 +0,0 @@
-package net.sf.taverna.t2.provenance;
-
-import net.sf.taverna.t2.provenance.connector.AbstractProvenanceConnector;
-
-public interface ProvenanceConnectorFactory {
-	public AbstractProvenanceConnector getProvenanceConnector();
-	public String getConnectorType();
-}
